45e047a8353c833195b515a4c7da4f2154934734
chen_h <chen_h@outlook.com>
2022-06-02 00:23:12 -0500

    Update 0_run.sh

--- scripts/0_run.sh
+++ scripts/0_run.sh
@@ -1,19 +1,4 @@
 #!bin/bash
 
-TEXT_RESET='\e[0m'
-TEXT_GREEN='\e[1;32m'
-TEXT_YELLOW='\e[1;33m'
-
-# check internet connection
-wget -q --spider http://google.com
-echo -e $TEXT_GREEN
-if [ $? -eq 0 ]
-then 
-  echo "Internet is connected, continue..."
-else 
-  echo "No internet connection, please first connect to internet then hit [Enter] to continue..."
-fi
-echo -e $TEXT_RESET
-
 # update
 bash 1_update.sh
