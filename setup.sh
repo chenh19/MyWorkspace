d939cc9460101689e1f187dd999754adbbcb4a77
chen_h <chen_h@outlook.com>
2022-06-01 23:14:30 -0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# download setup.sh
+
+
+# create cache directory and download setup scripts
+cd ~
+[ ! -d .setup_cache ] && mkdir .setup_cache
+cd ./.setup_cache
+[ ! -d master ] && wget https://codeload.github.com/chenh19/myworkspace/zip/refs/heads/main &&
+unzip -o -q main && rm main
+cd ./myworkspace-main && rm LICENSE README.md && cd ./scripts
+
+
+# update
+bash 1_update.sh
+
+
+# cleanup
+cd ../../../ && rm -rf ./.setup_cache
