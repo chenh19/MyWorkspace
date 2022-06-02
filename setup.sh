a4c57a5c92470725d0369afd2e730b0c1459cf9d
chen_h <chen_h@outlook.com>
2022-06-02 00:23:52 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# set promot color
+TEXT_RESET='\e[0m'
+TEXT_GREEN='\e[1;32m'
+TEXT_YELLOW='\e[1;33m'
+
+# check internet connection
+wget -q --spider http://google.com
+echo -e $TEXT_GREEN
+if [ $? -eq 0 ]
+then 
+  echo "Internet is connected, continue..."
+else 
+  echo "No internet connection, please first connect to internet then hit [Enter] to continue..."
+fi
+echo -e $TEXT_RESET
+
 # create cache directory
 cd ~
 [ ! -d .setup_cache ] && mkdir .setup_cache
