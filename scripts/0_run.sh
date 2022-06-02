9cc9e34f21ef06dae334ab3db5226b6d2de23440
chen_h <chen_h@outlook.com>
2022-06-02 00:11:10 -0500

    Update 0_run.sh

--- scripts/0_run.sh
+++ scripts/0_run.sh
@@ -1,8 +1,18 @@
 #!bin/bash
 
+TEXT_RESET='\e[0m'
+TEXT_YELLOW='\e[0;33m'
+TEXT_RED_B='\e[1;31m'
+
 # check internet connection
 wget -q --spider http://google.com
-if [ $? -eq 0 ]; then echo "Online"; else echo "Offline"; fi
+if [ $? -eq 0 ]
+then 
+  echo "Online"
+else 
+  echo "Offline"
+fi
+echo -e $TEXT_RESET
 
 # update
 bash 1_update.sh
