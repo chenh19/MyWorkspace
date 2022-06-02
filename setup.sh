56402906cefb4511edefc9f499e2ff73da5d6146
chen_h <chen_h@outlook.com>
2022-06-01 23:56:30 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# create cache directory and download setup scripts
+# create cache directory
 cd ~
 [ ! -d .setup_cache ] && mkdir .setup_cache
+
+# download setup scripts
 cd ./.setup_cache
 [ ! -d master ] && wget https://codeload.github.com/chenh19/myworkspace/zip/refs/heads/main && unzip -o -q main && rm main
 cd ./myworkspace-main && rm LICENSE README.md setup.sh && cd ./scripts
 
-# update
+# run
 bash 0_run.sh
 
 # cleanup
