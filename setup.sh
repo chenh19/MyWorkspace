c24872d48b52fabe8868ab333f564abe1399ffe3
chen_h <chen_h@outlook.com>
2022-06-02 07:38:17 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ cd ./myworkspace-main && rm LICENSE README.md setup.sh && cd ./scripts
 bash 0_run.sh
 
 # cleanup
-sleep 5
+sleep 10
 cd ../../../ && rm -rf ./.setup_cache
 echo -e $TEXT_YELLOW
 echo "All done!"
