aa60499924f8e687601e4794426007f37159135b
chen_h <chen_h@outlook.com>
2022-06-01 23:27:12 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,7 @@
 cd ~
 [ ! -d .setup_cache ] && mkdir .setup_cache
 cd ./.setup_cache
-[ ! -d master ] && wget https://codeload.github.com/chenh19/myworkspace/zip/refs/heads/main &&
-unzip -o -q main && rm main
+[ ! -d master ] && wget https://codeload.github.com/chenh19/myworkspace/zip/refs/heads/main && unzip -o -q main && rm main
 cd ./myworkspace-main && rm LICENSE README.md && cd ./scripts
 
 
