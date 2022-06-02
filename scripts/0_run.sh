45335fc352e5457a9503f169e7b43b36bb199660
chen_h <chen_h@outlook.com>
2022-06-02 01:02:36 -0500

    Update 0_run.sh

--- scripts/0_run.sh
+++ scripts/0_run.sh
@@ -1,4 +1,10 @@
 #!bin/bash
 
 # update
+echo -e $TEXT_YELLOW
+echo "System packages updating..."
+echo -e $TEXT_RESET
 bash 1_update.sh
+echo -e $TEXT_YELLOW
+echo "System packages update finished"
+echo -e $TEXT_RESET
