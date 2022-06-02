8a6b10b7a528b5d543de1b3f382a02ffa42909e0
chen_h <chen_h@outlook.com>
2022-06-02 08:00:18 -0500

    Update 0_run.sh

--- scripts/0_run.sh
+++ scripts/0_run.sh
@@ -4,9 +4,10 @@
 TEXT_RESET='\e[0m'
 TEXT_YELLOW='\e[1;33m'
 
+
 # update
 echo -e $TEXT_YELLOW
-echo "System packages updating..."
+echo "Updating system packages..."
 echo -e $TEXT_RESET
   bash 1_update.sh && sleep 1
 echo -e $TEXT_YELLOW
