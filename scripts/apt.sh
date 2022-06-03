23b124721c0e08306e16448e3aecc18beddf3883
chen_h <chen_h@outlook.com>
2022-06-03 10:37:21 -0500

    Add files via upload

---
+++ scripts/apt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# install java
+sudo apt-get update && sudo apt-get install default-jre default-jdk -y
+
+# install apps
+sudo apt-get install kwrite kcalc krita partitionmanager seahorse evolution evolution-ews xdotool kdocker -y
