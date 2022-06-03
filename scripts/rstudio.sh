d0ba10c3cf430ee19942a2eb2d2b773b34f4703f
chen_h <chen_h@outlook.com>
2022-06-03 14:45:24 -0500

    Update rstudio.sh

--- scripts/rstudio.sh
+++ scripts/rstudio.sh
@@ -15,7 +15,7 @@ sudo R CMD javareconf
 # install R packages and download web scraping driver using R scripts
 [ ! -d ./rscript ] && mkdir ./rscript
 echo "install.packages(c('devtools', 'BiocManager', 'tidyverse', 'readxl', 'writexl', 'expss', 'vcfR', 'filesstrings', 'R.utils', 'car', 'foreach', 'doParallel', 'rJava', 'RSelenium', 'base64enc', 'htmltools', 'markdown', 'rmarkdown', 'ggthemes', 'ggpubr', 'ggseqlogo', 'cowplot', 'pheatmap', 'Rtsne', 'umap'))" > ./rscript/packages.R
-echo "wdman::chrome(version = 'latest')" > ./rscript/webdriver.R
+echo -e "wdman::chrome(version = 'latest') \nBiocManager::install(c('GenomicRanges','qvalue'))\n" > ./rscript/webdriver.R
 sudo Rscript ./rscript/packages.R
 Rscript ./rscript/webdriver.R
 
