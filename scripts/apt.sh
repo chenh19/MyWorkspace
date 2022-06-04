3e7b97512d9964e2aa2983b4ce2f52280025a65b
chen_h <chen_h@outlook.com>
2022-06-03 22:24:18 -0500

    Update apt.sh

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -4,4 +4,4 @@
 sudo apt-get update && sudo apt-get install default-jre default-jdk -y
 
 # install apps
-sudo apt-get install kwrite kcalc krita partitionmanager seahorse evolution evolution-ews xdotool kdocker -y
+sudo apt-get install kwrite kcalc krita partitionmanager seahorse evolution evolution-ews xdotool kdocker curl -y
