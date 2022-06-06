8889572387ec6daf97b8896e4aaecbcb8ce5b1cf
Hang Chen <chen_h@outlook.com>
2022-06-06 15:13:13 -0500

    Update template.sh

--- src/template.sh
+++ src/template.sh
@@ -6,7 +6,6 @@ TEXT_RESET='\e[0m'
 TEXT_YELLOW='\e[1;33m'
 TEXT_GREEN='\e[1;32m'
 
-
 # set working directory
 [ ! -d ~/.setup_cache ] && mkdir ~/.setup_cache
 cd ~/.setup_cache
@@ -15,9 +14,7 @@ cd ~/.setup_cache
 # notify start
 echo -e " \n${TEXT_YELLOW}Starting...${TEXT_RESET} \n" && sleep 1
 
-
 # xxx
-
 
 # notify end
 echo -e " \n${TEXT_GREEN}Done!${TEXT_RESET} \n" && sleep 1
