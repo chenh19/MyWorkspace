6374ac3d426bc54456387423995133eedfc25771
Hang Chen <chen_h@outlook.com>
2022-06-06 15:15:16 -0500

    Update update.sh

--- src/update.sh
+++ src/update.sh
@@ -6,10 +6,15 @@ TEXT_RESET='\e[0m'
 TEXT_YELLOW='\e[1;33m'
 TEXT_GREEN='\e[1;32m'
 
+# set working directory
+[ ! -d ~/.setup_cache ] && mkdir ~/.setup_cache
+cd ~/.setup_cache
+
+
 # notify start
 echo -e " \n${TEXT_YELLOW}Updating system packages...${TEXT_RESET} \n" && sleep 1
 
-# apt update
+# apt update & cleanup
 sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get upgrade -y && sudo apt-get autoremove -y && sudo apt-get clean
 
 # flatpak update
