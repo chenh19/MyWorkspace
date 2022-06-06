b018ba6e8c8cd446c6565eaf46f8ff28793339e2
Hang Chen <chen_h@outlook.com>
2022-06-06 15:04:59 -0500

    Update deb.sh

--- src/deb.sh
+++ src/deb.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # This script installs deb packages by apt
 
+
 # set terminal font color
 TEXT_RESET='\e[0m'
 TEXT_YELLOW='\e[1;33m'
 TEXT_GREEN='\e[1;32m'
+
 
 # set working directory
 [ ! -d ~/.setup_cache ] && mkdir ~/.setup_cache
@@ -14,11 +16,14 @@ cd ~/.setup_cache
 # notify start
 echo -e " \n${TEXT_YELLOW}Deb packages installing...${TEXT_RESET} \n" && sleep 1
 
+
 # install java
 sudo apt-get update && sudo apt-get install default-jre default-jdk -y
 
+
 # install apps
 sudo apt-get install kwrite kcalc krita partitionmanager seahorse evolution evolution-ews xdotool kdocker curl -y
+
 
 # notify end
 echo -e " \n${TEXT_GREEN}Deb pacakges installed!${TEXT_RESET} \n" && sleep 1
