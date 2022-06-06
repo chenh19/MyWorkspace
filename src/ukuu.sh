6eadd4a9a1be6decee1b561a141789bbeb16ad69
Hang Chen <chen_h@outlook.com>
2022-06-06 15:14:31 -0500

    Update ukuu.sh

--- src/ukuu.sh
+++ src/ukuu.sh
@@ -6,6 +6,11 @@ TEXT_RESET='\e[0m'
 TEXT_YELLOW='\e[1;33m'
 TEXT_GREEN='\e[1;32m'
 
+# set working directory
+[ ! -d ~/.setup_cache ] && mkdir ~/.setup_cache
+cd ~/.setup_cache
+
+
 # notify start
 echo -e " \n${TEXT_YELLOW}Installing UKUU...${TEXT_RESET} \n" && sleep 1
 
@@ -15,6 +20,9 @@ wget -O - https://teejeetech.com/install-ukuu-8ALv9hCkUG.sh | bash
 # write license key
 [ ! -d ~/.config/ukuu/ ] && mkdir ~/.config/ukuu/
 echo -e "SGFuZyBDaGVufGNoZW5faEBvdXRsb29rLmNvbXwzUE9NLURMWkEtVFdIRy1ITzgwLUM2Nlg=" > ~/.config/ukuu/license.dat
+
+# cleanup
+sudo apt-get autoremove -y && sudo apt-get clean
 
 # notify end
 echo -e " \n${TEXT_GREEN}UKUU activated!${TEXT_RESET} \n" && sleep 1
