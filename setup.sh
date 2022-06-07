6deb50d2c9038d239b90d89260f9fafbc487ba51
Hang Chen <chen_h@outlook.com>
2022-06-06 19:25:12 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,10 +25,10 @@ echo -e " \n${TEXT_GREEN}All scripts downloaded${TEXT_RESET} \n" && sleep 1
 
 # setup
 bash ./src/deb.sh
-#bash ./src/flathub.sh
-#bash ./src/ukuu.sh
+bash ./src/flathub.sh
+bash ./src/ukuu.sh
 bash ./src/rstudio.sh
-#bash ./src/update.sh
+bash ./src/update.sh
 
 
 # final cleanup
