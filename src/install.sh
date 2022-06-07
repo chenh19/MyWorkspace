b398cb491cc9e32434cc569ec8900f401bd4bb0e
Hang Chen <chen_h@outlook.com>
2022-06-07 00:36:15 -0500

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-sudo chmod +x ./src/snapgene.sh
-bash ./src/snapgene.sh
+
+# set working directory
+[ ! -d ~/.setup_cache/ ] && mkdir ~/.setup_cache/
+cd ~/.setup_cache/
+
+chmod +x ~/setup_cache/src/snapgene.sh
+bash ~/setup_cache/src/snapgene.sh
