1462adc61b8e9107db638c28dbbc9c1dc5ce7cd7
Hang Chen <chen_h@outlook.com>
2022-06-06 20:49:35 -0500

    Update rstudio.sh

--- src/rstudio.sh
+++ src/rstudio.sh
@@ -35,7 +35,8 @@ Rscript ./rscript/webdriver.R
 # install RStudio
 [ ! -d ./rstudio/ ] && mkdir ./rstudio/
 wget -q https://download1.rstudio.org/desktop/jammy/amd64/rstudio-2022.02.3-492-amd64.deb #_to_be_updated
-mv -f ./*.deb ./rstudio/ && sudo dpkg -i ./rstudio/*.deb && sudo apt-get install -f -y
+mv -f ./*.deb ./rstudio/ && sudo dpkg -i ./rstudio/*.deb
+sudo apt-get -f -y install
 
 # cleanup
 sudo apt-get autoremove -y && sudo apt-get clean
