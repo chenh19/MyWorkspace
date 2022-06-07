5189098cf26324d2d05b08a6ff5a6c2892c54d0c
Hang Chen <chen_h@outlook.com>
2022-06-07 00:54:14 -0500

    Delete install.sh

--- src/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# set working directory
-[ ! -d ~/.setup_cache/ ] && mkdir ~/.setup_cache/
-cd ~/.setup_cache/
-
-chmod u+x ~/.setup_cache/src/snapgene.sh
-bash ~/.setup_cache/src/snapgene.sh
