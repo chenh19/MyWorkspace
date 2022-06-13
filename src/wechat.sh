2fbd76864328690c86d664be934134c98a79f962
Hang Chen <chen_h@outlook.com>
2022-06-13 03:46:15 -0500

    Update wechat.sh

--- src/wechat.sh
+++ src/wechat.sh
@@ -36,8 +36,7 @@ case "$choice" in
         "/opt/deepinwine/apps/Deepin-WeChat/run.sh" -u %u && sleep 10 && killall -9 WeChat.exe
         
         # configure scaling
-        echo ""
-        echo -e "${TEXT_YELLOW}In the popup window, please navigate to [Graphics] tab.${TEXT_RESET} \n"
+        echo -e " \n${TEXT_YELLOW}In the popup window, please navigate to [Graphics] tab.${TEXT_RESET} \n"
         echo -e "${TEXT_YELLOW}Set [Screen resolution] to 200dpi, then click [OK] to exit.${TEXT_RESET} \n"
         env WINEPREFIX="$HOME/.deepinwine/Deepin-WeChat" /usr/bin/deepin-wine winecfg 
         #sudo sed -i 's+"LogPixels"=dword:00000060+"LogPixels"=dword:000000c8+g' ~/.deepinwine/Deepin-WeChat/system.reg
