d2b65f573b9bf8092014cf5c56362f5b6d459e8d
Hang Chen <chen_h@outlook.com>
2022-06-12 23:42:42 -0500

    Update deb.sh

--- src/deb.sh
+++ src/deb.sh
@@ -23,7 +23,7 @@ sudo apt-get update && sudo apt-get install default-jre default-jdk -y
 
 # install apps (directly)
 # installed by Kubuntu by defauly: python3, git, kate, kcalc, partitionmanager
-sudo apt-get install kwrite krita seahorse evolution evolution-ews xdotool kdocker curl axel python3-pip -y
+sudo apt-get install kwrite krita seahorse evolution evolution-ews xdotool kdocker curl python3-pip -y
 
 # install apps (downloaded)
 [ ! -d ./deb/ ] && mkdir ./deb/
