b6b2603de2e4104965929c1375d6e70bcd989969
Hang Chen <chen_h@outlook.com>
2022-07-05 01:18:57 -0500

    Update deb.sh

--- src/deb.sh
+++ src/deb.sh
@@ -26,40 +26,44 @@ sudo apt-get update && sudo apt-get install default-jre default-jdk -y
 sudo apt-get install kwrite krita seahorse evolution evolution-ews xdotool kdocker curl python3-pip -y
 #sudo apt-get install axel -y
 
-# install apps (downloaded)
-[ ! -d ./deb/ ] && mkdir ./deb/
-wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget -q https://zoom.us/client/latest/zoom_amd64.deb
-wget -q https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+# install apps (source list)
+  # enpass
+  echo "deb https://apt.enpass.io/ stable main" | sudo tee /etc/apt/sources.list.d/enpass.list
+  wget -O - https://apt.enpass.io/keys/enpass-linux.key | sudo tee /etc/apt/trusted.gpg.d/enpass.asc
+  sudo apt-get update && sudo apt-get install enpass -y
 
-wget -q https://github.com/JoseExposito/touchegg/releases/download/2.0.14/touchegg_2.0.14_amd64.deb #_to_be_updated
-wget -q https://downloads.slack-edge.com/releases/linux/4.27.154/prod/x64/slack-desktop-4.27.154-amd64.deb #_to_be_updated
-wget -q https://github.com/Automattic/simplenote-electron/releases/download/v2.21.0/Simplenote-linux-2.21.0-amd64.deb #_to_be_updated
-wget -q https://github.com/jurplel/qView/releases/download/5.0/qview_5.0.1-focal4_amd64.deb #_to_be_updated
-mv -f ./*.deb ./deb/ && sudo apt-get install -f -y ./deb/*.deb
+# install apps (downloaded)
+  [ ! -d ./deb/ ] && mkdir ./deb/
+  wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  wget -q https://zoom.us/client/latest/zoom_amd64.deb
+  wget -q https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+  wget -q https://github.com/JoseExposito/touchegg/releases/download/2.0.14/touchegg_2.0.14_amd64.deb #_to_be_updated
+  wget -q https://downloads.slack-edge.com/releases/linux/4.27.154/prod/x64/slack-desktop-4.27.154-amd64.deb #_to_be_updated
+  wget -q https://github.com/Automattic/simplenote-electron/releases/download/v2.21.0/Simplenote-linux-2.21.0-amd64.deb #_to_be_updated
+  wget -q https://github.com/jurplel/qView/releases/download/5.0/qview_5.0.1-focal4_amd64.deb #_to_be_updated
+  mv -f ./*.deb ./deb/ && sudo apt-get install -f -y ./deb/*.deb
 
 # config
-# Start Slack minimized
-sudo sed -i 's+Exec=/usr/bin/slack %U+Exec=/usr/bin/slack -u %U+g' /usr/share/applications/slack.desktop
-# right click on the Slack icon on the bottom right, click "Check for Updates..." to open login interface, check "Launch on Login" if preferred
-# zoom auto scaling
-kwriteconfig5 --file ~/.config/zoomus.conf --group General --key autoScale "false"
-# teamviewer wallpaper
-[ -d ~/.config/teamviewer/ ] && rm -rf ~/.config/teamviewer/
-cp -rf ./src/teamviewer/ ~/.config/
-# simplenote quites unexpectedly
-sudo sed -i 's+Exec=/opt/Simplenote/simplenote %U+Exec=/opt/Simplenote/simplenote --no-sandbox %U+g' /usr/share/applications/simplenote.desktop
-# qView
-[ ! -d ~/.config/qView/ ] && mkdir ~/.config/qView/
-kwriteconfig5 --file ~/.config/qView/qView.conf --group options --key updatenotifications "false"
-kwriteconfig5 --file ~/.config/qView/qView.conf --group options --key loopfoldersenabled "false"
-kwriteconfig5 --file ~/.config/qView/qView.conf --group options --key titlebarmode "2"
-# do not check update notification when first open
-
-# Enpass
-echo "deb https://apt.enpass.io/ stable main" | sudo tee /etc/apt/sources.list.d/enpass.list
-wget -O - https://apt.enpass.io/keys/enpass-linux.key | sudo tee /etc/apt/trusted.gpg.d/enpass.asc
-sudo apt-get update && sudo apt-get install enpass -y
+  # Start Slack minimized
+    sudo sed -i 's+Exec=/usr/bin/slack %U+Exec=/usr/bin/slack -u %U+g' /usr/share/applications/slack.desktop
+    # right click on the Slack icon on the bottom right, click "Check for Updates..." to open login interface, check "Launch on Login" if preferred
+  
+  # zoom auto scaling
+    kwriteconfig5 --file ~/.config/zoomus.conf --group General --key autoScale "false"
+  
+  # teamviewer wallpaper
+    [ -d ~/.config/teamviewer/ ] && rm -rf ~/.config/teamviewer/
+    cp -rf ./src/teamviewer/ ~/.config/
+  
+  # simplenote quites unexpectedly
+    sudo sed -i 's+Exec=/opt/Simplenote/simplenote %U+Exec=/opt/Simplenote/simplenote --no-sandbox %U+g' /usr/share/applications/simplenote.desktop
+  
+  # qView
+    [ ! -d ~/.config/qView/ ] && mkdir ~/.config/qView/
+    kwriteconfig5 --file ~/.config/qView/qView.conf --group options --key updatenotifications "false"
+    kwriteconfig5 --file ~/.config/qView/qView.conf --group options --key loopfoldersenabled "false"
+    kwriteconfig5 --file ~/.config/qView/qView.conf --group options --key titlebarmode "2"
+    # do not check update notification when first open
 
 
 # cleanup
