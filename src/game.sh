f9a1504bd10b96424ea29f8f49aec06161a5f468
Hang Chen <chen_h@outlook.com>
2022-07-05 12:17:39 -0500

    Add files via upload

---
+++ src/game.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# This script does xxx
+
+# set terminal font color
+TEXT_YELLOW='\e[1;33m'
+TEXT_GREEN='\e[1;32m'
+TEXT_RESET='\e[0m'
+
+# set working directory
+[ ! -d ~/.setup_cache/ ] && mkdir ~/.setup_cache/
+cd ~/.setup_cache/
+
+
+# notify start
+sudo echo ""
+echo -e "${TEXT_YELLOW}Installing games...${TEXT_RESET} \n" && sleep 1
+
+# human resource machine
+wget -q https://www.dropbox.com/s/4f804e3873e0wq7/HumanResourceMachine.zip?dl=0 && sleep 5
+unzip -o -q HumanResourceMachine.zip?dl=0 && sleep 1 && rm HumanResourceMachine.zip?dl=0 && sleep 1
+bash ./HumanResourceMachine-Linux-2016-03-23.sh # change install path if needed
+
+
+# cleanup
+rm ./HumanResourceMachine-Linux-2016-03-23.sh
+
+# notify end
+echo -e " \n${TEXT_GREEN}Done!${TEXT_RESET} \n" && sleep 5
+
+# mark setup.sh
+sed -i 's+bash ./src/game.sh+#bash ./src/game.sh+g' ~/.setup_cache/setup.sh
