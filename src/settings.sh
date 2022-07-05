a135e3def55e83f55e16da59fb9c9a68fc20350a
Hang Chen <chen_h@outlook.com>
2022-07-04 22:16:43 -0500

    Update settings.sh

--- src/settings.sh
+++ src/settings.sh
@@ -33,24 +33,24 @@ case "$choice" in
 esac
 
 ## System Settings > Appearance > Window Decorations > Titlebar Buttons > drag and remove "On all desktops"
-kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key ButtonsOnLeft "M"
-kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key ButtonsOnRight "IAX"
+kwriteconfig5 --file ~/.config/kwinrc --group org.kde.kdecoration2 --key ButtonsOnLeft "M"
+kwriteconfig5 --file ~/.config/kwinrc --group org.kde.kdecoration2 --key ButtonsOnRight "IAX"
 
 ## System Settings > Appearance > Cursors > Size > 48 (take effect after rebooting)
-kwriteconfig5 --file $HOME/.config/kcminputrc --group Mouse --key cursorSize "48"
+kwriteconfig5 --file ~/.config/kcminputrc --group Mouse --key cursorSize "48"
 
 ######################################################################################
 
 # Workspace Behavior
 
 ## System Settings > Workspace Behavior > Desktop effects > uncheck "screen edge"
-kwriteconfig5 --file $HOME/.config/kwinrc --group Plugins --key screenedgeEnabled "false"
+kwriteconfig5 --file ~/.config/kwinrc --group Plugins --key screenedgeEnabled "false"
 
 ## System Settings > Workspace Behavior > Screen Edges > "no actiion" for all corners
-kwriteconfig5 --file $HOME/.config/kwinrc --group Effect-PresentWindows --key BorderActivateAll "9"
+kwriteconfig5 --file ~/.config/kwinrc --group Effect-PresentWindows --key BorderActivateAll "9"
 
 ## System Settings > Workspace Behavior > Virtual Desktop > uncheck "Navigation wraps around"
-kwriteconfig5 --file $HOME/.config/kwinrc --group Windows --key RollOverDesktops "false"
+kwriteconfig5 --file ~/.config/kwinrc --group Windows --key RollOverDesktops "false"
 
 ######################################################################################
 
@@ -58,33 +58,33 @@ kwriteconfig5 --file $HOME/.config/kwinrc --group Windows --key RollOverDesktops
 
 ## System Settings > Shortcuts > Shortcuts > Add Application: 
 ### Konsole: Meta+R
-kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group org.kde.konsole.desktop --key NewTab "none,none,Open a New Tab"
-kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group org.kde.konsole.desktop --key NewWindow "Meta+R,none,Open a New Window"
-kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group org.kde.konsole.desktop --key _k_friendly_name "Konsole"
-kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group org.kde.konsole.desktop --key _launch "none,none,Konsole"
+kwriteconfig5 --file ~/.config/kglobalshortcutsrc --group org.kde.konsole.desktop --key NewTab "none,none,Open a New Tab"
+kwriteconfig5 --file ~/.config/kglobalshortcutsrc --group org.kde.konsole.desktop --key NewWindow "Meta+R,none,Open a New Window"
+kwriteconfig5 --file ~/.config/kglobalshortcutsrc --group org.kde.konsole.desktop --key _k_friendly_name "Konsole"
+kwriteconfig5 --file ~/.config/kglobalshortcutsrc --group org.kde.konsole.desktop --key _launch "none,none,Konsole"
 
 ### System Monitor: Ctrl+Shift+Esc
-kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group org.kde.plasma-systemmonitor.desktop --key _k_friendly_name "System Monitor"
-kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group org.kde.plasma-systemmonitor.desktop --key _launch "Ctrl+Shift+Esc,none,System Monitor"
+kwriteconfig5 --file ~/.config/kglobalshortcutsrc --group org.kde.plasma-systemmonitor.desktop --key _k_friendly_name "System Monitor"
+kwriteconfig5 --file ~/.config/kglobalshortcutsrc --group org.kde.plasma-systemmonitor.desktop --key _launch "Ctrl+Shift+Esc,none,System Monitor"
 
 ## Workspace Behavior > Activities > Switching > Customize two Global shortcuts as "None"
-kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group plasmashell --key 'manage activities' "none,none,Show Activity Switcher"
-kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group plasmashell --key 'next activity' "none,none,Walk through activities"
-kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group plasmashell --key 'previous activity' "none,none,Walk through activities (Reverse)"
+kwriteconfig5 --file ~/.config/kglobalshortcutsrc --group plasmashell --key 'manage activities' "none,none,Show Activity Switcher"
+kwriteconfig5 --file ~/.config/kglobalshortcutsrc --group plasmashell --key 'next activity' "none,none,Walk through activities"
+kwriteconfig5 --file ~/.config/kglobalshortcutsrc --group plasmashell --key 'previous activity' "none,none,Walk through activities (Reverse)"
 
 ## System Settings > Shortcuts > Shortcuts > KWin:
 ### Close Window > Meta+Q
-kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group kwin --key 'Window Close' "Meta+Q\tAlt+F4,Alt+F4,Close Window"
+kwriteconfig5 --file ~/.config/kglobalshortcutsrc --group kwin --key 'Window Close' "Meta+Q\tAlt+F4,Alt+F4,Close Window"
 sed -i '/Window Close/s/\\\\t/\\t/g' ~/.config/kglobalshortcutsrc
 
 ### Switch One Desktop to the Left: Meta+Ctrl+Left
-kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group kwin --key 'Switch One Desktop to the Left' "Meta+Ctrl+Left,Meta+Ctrl+Left,Switch One Desktop to the Left"
+kwriteconfig5 --file ~/.config/kglobalshortcutsrc --group kwin --key 'Switch One Desktop to the Left' "Meta+Ctrl+Left,Meta+Ctrl+Left,Switch One Desktop to the Left"
 
 ### Switch One Desktop to the Right: Meta+Ctrl+Right
-kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group kwin --key 'Switch One Desktop to the Right' "Meta+Ctrl+Right,Meta+Ctrl+Right,Switch One Desktop to the Right"
+kwriteconfig5 --file ~/.config/kglobalshortcutsrc --group kwin --key 'Switch One Desktop to the Right' "Meta+Ctrl+Right,Meta+Ctrl+Right,Switch One Desktop to the Right"
 
 ### Toggle Overview: Meta+Tab
-kwriteconfig5 --file $HOME/.config/kglobalshortcutsrc --group kwin --key 'Overview' "Meta+Tab\tMeta+W,Meta+W,Toggle Overview"
+kwriteconfig5 --file ~/.config/kglobalshortcutsrc --group kwin --key 'Overview' "Meta+Tab\tMeta+W,Meta+W,Toggle Overview"
 sed -i '/Overview/s/\\\\t/\\t/g' ~/.config/kglobalshortcutsrc
 
 ######################################################################################
@@ -92,30 +92,30 @@ sed -i '/Overview/s/\\\\t/\\t/g' ~/.config/kglobalshortcutsrc
 # Startup and Shutdown
 
 ## System Settings > Startup and Shutdown > Desktop Session > When logging in > Start with an empty session
-kwriteconfig5 --file $HOME/.config/ksmserverrc --group General --key loginMode "emptySession"
+kwriteconfig5 --file ~/.config/ksmserverrc --group General --key loginMode "emptySession"
 
 ######################################################################################
 
 # Search
 
 ## System Settings > Search > File Search > check "Enable File Search"
-kwriteconfig5 --file $HOME/.config/baloofilerc --group Basic --key Indexing-Enabled "Settings"
-kwriteconfig5 --file $HOME/.config/baloofilerc --group 'Basic Settings' --key Indexing-Enabled "true"
-kwriteconfig5 --file $HOME/.config/krunnerrc --group PlasmaRunnerManager --key migrated "true"
-kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key baloosearchEnabled "true"
+kwriteconfig5 --file ~/.config/baloofilerc --group Basic --key Indexing-Enabled "Settings"
+kwriteconfig5 --file ~/.config/baloofilerc --group 'Basic Settings' --key Indexing-Enabled "true"
+kwriteconfig5 --file ~/.config/krunnerrc --group PlasmaRunnerManager --key migrated "true"
+kwriteconfig5 --file ~/.config/krunnerrc --group Plugins --key baloosearchEnabled "true"
 
 ## System Settings > Search > Plasma Search > KRunner position: Center
-kwriteconfig5 --file $HOME/.config/krunnerrc --group General --key FreeFloating "true"
+kwriteconfig5 --file ~/.config/krunnerrc --group General --key FreeFloating "true"
 
 ## System Settings > Search > Plasma Search > KRunner history: uncheck "Retain previous search"
-kwriteconfig5 --file $HOME/.config/krunnerrc --group General --key RetainPriorSearch "false"
+kwriteconfig5 --file ~/.config/krunnerrc --group General --key RetainPriorSearch "false"
 
 ######################################################################################
 
 # Notification
 
 ## System Settings > Notification > Configure > Other Applications > uncheck "Show popups"
-kwriteconfig5 --file $HOME/.config/plasmanotifyrc --group 'Applications' --group '@other' --key 'ShowPopups' "false"
+kwriteconfig5 --file ~/.config/plasmanotifyrc --group 'Applications' --group '@other' --key 'ShowPopups' "false"
 
 ######################################################################################
 
@@ -160,7 +160,7 @@ kwriteconfig5 --file ~/.config/plasmashellrc --group 'PlasmaViews' --group 'Pane
 favorites=$(grep 'Favorites-org.kde.plasma.kickoff.favorites.' ~/.config/kactivitymanagerd-statsrc | sed 's/\[//g' | sed 's/\]//g')
 for favorite in $favorites
 do
-    kwriteconfig5 --file $HOME/.config/kactivitymanagerd-statsrc --group $favorite --key ordering "applications:org.kde.dolphin.desktop,applications:org.kde.konsole.desktop"
+    kwriteconfig5 --file ~/.config/kactivitymanagerd-statsrc --group $favorite --key ordering "applications:org.kde.dolphin.desktop,applications:org.kde.konsole.desktop"
 done
 
 ######################################################################################
@@ -199,31 +199,31 @@ echo -e "${TEXT_YELLOW}Configuring applications...${TEXT_RESET} \n" && sleep 1
 # Konsole
 
 ## Konsole window height 32
-kwriteconfig5 --file $HOME/.config/konsolerc --group MainWindow --key 'eDP-1 Height 1536x864' "625"
-kwriteconfig5 --file $HOME/.config/konsolerc --group MainWindow --key 'eDP-1 Height 1536x960' "625"
-kwriteconfig5 --file $HOME/.config/konsolerc --group MainWindow --key 'eDP-1 Height 1920x1080' "625"
-kwriteconfig5 --file $HOME/.config/konsolerc --group MainWindow --key 'eDP-1 Height 1920x1200' "625"
+kwriteconfig5 --file ~/.config/konsolerc --group MainWindow --key 'eDP-1 Height 1536x864' "625"
+kwriteconfig5 --file ~/.config/konsolerc --group MainWindow --key 'eDP-1 Height 1536x960' "625"
+kwriteconfig5 --file ~/.config/konsolerc --group MainWindow --key 'eDP-1 Height 1920x1080' "625"
+kwriteconfig5 --file ~/.config/konsolerc --group MainWindow --key 'eDP-1 Height 1920x1200' "625"
 
 ## Konsole window height 96
-kwriteconfig5 --file $HOME/.config/konsolerc --group MainWindow --key 'eDP-1 Width 1536x864' "803"
-kwriteconfig5 --file $HOME/.config/konsolerc --group MainWindow --key 'eDP-1 Width 1536x960' "803"
-kwriteconfig5 --file $HOME/.config/konsolerc --group MainWindow --key 'eDP-1 Width 1920x1080' "803"
-kwriteconfig5 --file $HOME/.config/konsolerc --group MainWindow --key 'eDP-1 Width 1920x1200' "803"
+kwriteconfig5 --file ~/.config/konsolerc --group MainWindow --key 'eDP-1 Width 1536x864' "803"
+kwriteconfig5 --file ~/.config/konsolerc --group MainWindow --key 'eDP-1 Width 1536x960' "803"
+kwriteconfig5 --file ~/.config/konsolerc --group MainWindow --key 'eDP-1 Width 1920x1080' "803"
+kwriteconfig5 --file ~/.config/konsolerc --group MainWindow --key 'eDP-1 Width 1920x1200' "803"
 
 ## konsole window show "Main Toolbar" and "Session Toolbar"
-kwriteconfig5 --file $HOME/.config/konsolerc --group MainWindow --key State "AAAA/wAAAAD9AAAAAQAAAAAAAAAAAAAAAPwCAAAAAfsAAAAcAFMAUwBIAE0AYQBuAGEAZwBlAHIARABvAGMAawAAAAAA/////wAAANUBAAADAAADIwAAAiUAAAAEAAAABAAAAAgAAAAI/AAAAAEAAAACAAAAAgAAABYAbQBhAGkAbgBUAG8AbwBsAEIAYQByAQAAAAD/////AAAAAAAAAAAAAAAcAHMAZQBzAHMAaQBvAG4AVABvAG8AbABiAGEAcgEAAADo/////wAAAAAAAAAA"
+kwriteconfig5 --file ~/.config/konsolerc --group MainWindow --key State "AAAA/wAAAAD9AAAAAQAAAAAAAAAAAAAAAPwCAAAAAfsAAAAcAFMAUwBIAE0AYQBuAGEAZwBlAHIARABvAGMAawAAAAAA/////wAAANUBAAADAAADIwAAAiUAAAAEAAAABAAAAAgAAAAI/AAAAAEAAAACAAAAAgAAABYAbQBhAGkAbgBUAG8AbwBsAEIAYQByAQAAAAD/////AAAAAAAAAAAAAAAcAHMAZQBzAHMAaQBvAG4AVABvAG8AbABiAGEAcgEAAADo/////wAAAAAAAAAA"
 
 ######################################################################################
 
 ## System Monitor
 #Edit or remove pages: uncheck "Applications" and list in this order: "Overview", "Processes", "History"
-kwriteconfig5 --file $HOME/.config/systemmonitorrc --group General --key 'hiddenPages' "applications.page"
-kwriteconfig5 --file $HOME/.config/systemmonitorrc --group General --key 'pageOrder' "overview.page,process.page,history.page,application.page"
+kwriteconfig5 --file ~/.config/systemmonitorrc --group General --key 'hiddenPages' "applications.page"
+kwriteconfig5 --file ~/.config/systemmonitorrc --group General --key 'pageOrder' "overview.page,process.page,history.page,application.page"
 
 ######################################################################################
 
 # spectacle
-kwriteconfig5 --file $HOME/.config/spectaclerc --group GuiConfig --key 'quitAfterSaveCopyExport' "true"
+kwriteconfig5 --file ~/.config/spectaclerc --group GuiConfig --key 'quitAfterSaveCopyExport' "true"
 
 ######################################################################################
 
@@ -231,10 +231,10 @@ kwriteconfig5 --file $HOME/.config/spectaclerc --group GuiConfig --key 'quitAfte
 [ -f ~/.config/okularpartrc ] && rm ~/.config/okularpartrc
 [ -f ~/.config/okularrc ] && rm ~/.config/okularrc
 touch ~/.config/okularpartrc ~/.config/okularrc
-kwriteconfig5 --file $HOME/.config/okularpartrc --group General --key 'ShellOpenFileInTabs' "true"
-kwriteconfig5 --file $HOME/.config/okularpartrc --group 'Core Performance' --key 'MemoryLevel' "Greedy"
-kwriteconfig5 --file $HOME/.config/okularpartrc --group 'Dlg Presentation' --key 'SlidesShowProgress' "false"
-kwriteconfig5 --file $HOME/.config/okularrc --group 'Notification Messages' --key 'ShowTabWarning' "false"
+kwriteconfig5 --file ~/.config/okularpartrc --group General --key 'ShellOpenFileInTabs' "true"
+kwriteconfig5 --file ~/.config/okularpartrc --group 'Core Performance' --key 'MemoryLevel' "Greedy"
+kwriteconfig5 --file ~/.config/okularpartrc --group 'Dlg Presentation' --key 'SlidesShowProgress' "false"
+kwriteconfig5 --file ~/.config/okularrc --group 'Notification Messages' --key 'ShowTabWarning' "false"
 [ -d ~/.local/share/kxmlgui5/okular/ ] && rm -rf ~/.local/share/kxmlgui5/okular/
 cp -rf ./src/okular/ ~/.local/share/kxmlgui5/
 
