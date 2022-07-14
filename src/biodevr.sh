1a6ab91251b2f03be77e63a023202f305bd2d334
Hang Chen <chen_h@outlook.com>
2022-07-14 03:26:47 -0500

    Update biodevr.sh

--- src/biodevr.sh
+++ src/biodevr.sh
@@ -51,6 +51,11 @@ case "$choice" in
                 sleep 1 && mv -f ./*.deb ./devdeb/ && sudo dpkg -i ./devdeb/*.deb && sleep 1
                 sudo apt-get -f -y install
                 
+                # config
+                [ ! -d ~/.config/rstudio/ ] && mkdir ~/.config/rstudio/
+                echo -e 'crash-handling-enabled="0"' > ~/.config/rstudio/crash-handler.conf
+                echo -e '{ \n    "initial_working_directory": "~", \n    "posix_terminal_shell": "bash", \n    "editor_theme": "Tomorrow Night", \n    "pdf_previewer": "none" \n}' > ~/.config/rstudio/rstudio-prefs.json
+                
                 # notify end
                 echo -e " \n${TEXT_GREEN}RStudio installed!${TEXT_RESET} \n" && sleep 5;;
                 
