ad96c319ad9a7a4c4a25af5e18dd8b40624015fb
Hang Chen <chen_h@outlook.com>
2022-07-14 05:26:57 -0500

    Update biodevpy.sh

--- src/biodevpy.sh
+++ src/biodevpy.sh
@@ -28,13 +28,14 @@ case "$choice" in
         conda config --add channels defaults
         conda config --add channels bioconda
         conda config --add channels conda-forge
+        conda install numpy -y
         #conda install kallisto -y
-        #conda install numpy -y
+        
+        # config
         conda config --set auto_activate_base false # disable auto activate base in terminal
         #conda activate # activate base when needed
         #rm -rf ~/anaconda3/ # uninstall anaconda
         # note: conda installed packages are constrained to the conda environment, while pip3 installed packages might affect system environment
-        
         
         # ask whether to install Jupyter Lab
         sudo echo ""
