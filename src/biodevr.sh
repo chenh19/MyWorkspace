f442e12ced4cce29bc8fad3460474d6d441d7b17
Hang Chen <chen_h@outlook.com>
2022-07-14 02:40:21 -0500

    Update biodevr.sh

--- src/biodevr.sh
+++ src/biodevr.sh
@@ -48,8 +48,8 @@ case "$choice" in
                 # install RStudio
                 [ ! -d ./devdeb/ ] && mkdir ./devdeb/
                 wget -q https://download1.rstudio.org/desktop/jammy/amd64/rstudio-2022.07.0-548-amd64.deb #_to_be_updated
-                sleep 1 && mv -f ./*.deb ./devdeb/ && sudo apt-get install -y./devdeb/*.deb && sleep 1
-                sudo apt-get -f -y install #sudo apt-get --fix-broken install -y
+                sleep 1 && mv -f ./*.deb ./devdeb/ && sudo dpkg -i ./devdeb/*.deb && sleep 1
+                sudo apt-get -f -y install
                 
                 # notify end
                 echo -e " \n${TEXT_GREEN}RStudio installed!${TEXT_RESET} \n" && sleep 5;;
