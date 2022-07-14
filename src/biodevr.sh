b6f8637c490c56cd93f828a0861a70b7e4bd4ccd
Hang Chen <chen_h@outlook.com>
2022-07-13 21:55:47 -0500

    Update biodevr.sh

--- src/biodevr.sh
+++ src/biodevr.sh
@@ -16,7 +16,7 @@ read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to set up R environment
 case "$choice" in
   y|Y ) # notify start
         sudo echo ""
-        echo -e "${TEXT_YELLOW}Installing R bioinfomatics developing enviroment...${TEXT_RESET} \n" && sleep 1
+        echo -e "${TEXT_YELLOW}Installing R bioinfomatics developing enviroment (this might take a while)...${TEXT_RESET} \n" && sleep 1
 
         # update system and install packages required by R and R packages installing
         sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install default-jre default-jdk libxml2-dev libssl-dev libcurl4-openssl-dev libnlopt-dev libgeos-dev texlive-latex-extra -y
@@ -35,6 +35,47 @@ case "$choice" in
         sudo Rscript ./rscript/packages.R
         Rscript ./rscript/webdriver.R
         
+        
+        # ask whether to install RStudio
+        read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to install RStudio? [y/n/c]'$TEXT_RESET)"$' \n' choice
+        case "$choice" in
+          y|Y ) # notify start
+                sudo echo ""
+                echo -e "${TEXT_YELLOW}Installing RStudio...${TEXT_RESET} \n" && sleep 1
+                
+                # install RStudio
+                [ ! -d ./devdeb/ ] && mkdir ./devdeb/
+                wget -q https://download1.rstudio.org/desktop/jammy/amd64/rstudio-2022.07.0-548-amd64.deb #_to_be_updated
+                sleep 1 && mv -f ./*.deb ./devdeb/ && sudo dpkg -i ./devdeb/*.deb && sleep 1 && sudo apt-get -f -y install
+                
+                # notify end
+                echo -e " \n${TEXT_GREEN}RStudio installed!${TEXT_RESET} \n" && sleep 5;;
+                
+          * ) # notify cancellation
+                echo -e " \n${TEXT_YELLOW}RStudio not installed.${TEXT_RESET} \n" && sleep 5;;
+        
+        esac
+        
+        # ask whether to install RKWard
+        read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to install RKWard? [y/n/c]'$TEXT_RESET)"$' \n' choice
+        case "$choice" in
+          y|Y ) # notify start
+                sudo echo ""
+                echo -e "${TEXT_YELLOW}Installing RKWard...${TEXT_RESET} \n" && sleep 1
+                
+                # install RKWard
+                # rkward is dependent on kate, which is installed by Kubuntu by default but usually uninstalled by me
+                sudo apt-get install kate kbibtex rkward -y;;
+                
+                # notify end
+                echo -e " \n${TEXT_GREEN}RKWard installed!${TEXT_RESET} \n" && sleep 5;;
+                
+          * ) # notify cancellation
+                echo -e " \n${TEXT_YELLOW}RKWard not installed.${TEXT_RESET} \n" && sleep 5;;
+        
+        esac
+        
+        
         # notify end
         echo -e " \n${TEXT_GREEN}R bioinformatic developing environment ready!${TEXT_RESET} \n" && sleep 5;;
 
@@ -46,26 +87,11 @@ case "$choice" in
 
 esac
 
-# ask whether to set up R environment for Bioinformatic developing
-read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to set up R environment for bioinformatic developing? [y/n/c]'$TEXT_RESET)"$' \n' choice
-case "$choice" in
-  y|Y ) # notify start
-        sudo echo ""
-        echo -e "${TEXT_YELLOW}Installing R bioinfomatics developing enviroment...${TEXT_RESET} \n" && sleep 1
-        
-        # install RKWard and fastqc
-        # rkward is dependent on kate, which is installed by Kubuntu by default but usually uninstalled by me
-        sudo apt-get install kate kbibtex rkward -y
-        
-        # install RStudio
-        [ ! -d ./devdeb/ ] && mkdir ./devdeb/
-        wget -q https://download1.rstudio.org/desktop/jammy/amd64/rstudio-2022.07.0-548-amd64.deb #_to_be_updated
-        sleep 1 && mv -f ./*.deb ./devdeb/ && sudo dpkg -i ./devdeb/*.deb && sleep 1
-        sudo apt-get -f -y install
 
-        # cleanup
-        sudo apt-get autoremove -y && sudo apt-get clean
-        rm -rf ./rscript/ ./devdeb/ ./shscript/
+# cleanup
+sudo apt-get autoremove -y && sudo apt-get clean
+[ ! -d ./rscript/ ] && rm -rf ./rscript/
+[ ! -d ./devdeb/ ] && rm -rf ./devdeb/
 
 # mark setup.sh
 sed -i 's+bash ./src/biodev.sh+#bash ./src/biodev.sh+g' ~/.setup_cache/setup.sh
