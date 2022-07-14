d00ed40db9248bbc6124f72242a9e8f9517fca32
Hang Chen <chen_h@outlook.com>
2022-07-14 04:42:51 -0500

    Update widgets.sh

--- src/widgets.sh
+++ src/widgets.sh
@@ -16,11 +16,13 @@ sudo echo ""
 echo -e "${TEXT_YELLOW}Installing widgets...${TEXT_RESET} \n" && sleep 1
 
 # install plasma widgets
-#sudo add-apt-repository ppa:kubuntu-ppa/backports && sudo apt-get dist-upgrade -y
-#/usr/lib/x86_64-linux-gnu/libexec/kf5/kpackagehandlers/knshandler kns://plasmoids.knsrc/api.kde-look.org/1704465 # text command
-#/usr/lib/x86_64-linux-gnu/libexec/kf5/kpackagehandlers/knshandler kns://plasmoids.knsrc/api.kde-look.org/1245902 # better inline clock
-#/usr/lib/x86_64-linux-gnu/libexec/kf5/kpackagehandlers/knshandler kns://plasmoids.knsrc/api.kde-look.org/1274218 # Window Title Applet
-#/usr/lib/x86_64-linux-gnu/libexec/kf5/kpackagehandlers/knshandler kns://plasmoids.knsrc/api.kde-look.org/1274975 # Window AppMenu Applet (unity like)
+#/usr/lib/x86_64-linux-gnu/libexec/kf5/kpackagehandlers/knshandler kns://plasmoids.knsrc/api.kde-look.org/1704465 # doesn't work for now
+wget -q https://www.dropbox.com/s/6n5g9a8q5etvtx5/adhe.textcommand.zip?dl=0 && sleep 1
+unzip -o -q *.zip?dl=0 && sleep 1 && rm *.zip?dl=0
+[ ! -d ~/.local/share.plasma/ ] && mkdir ~/.local/share.plasma/
+[ ! -d ~/.local/share.plasma/plasmoids/ ] && mkdir ~/.local/share.plasma/plasmoids/
+cp -rf ./adhe.textcommand/ ~/.local/share.plasma/plasmoids/
+rm -rf ./adhe.textcommand/
 
 # install dolphin widgets
 [ ! -d ~/.local/share/kservices5/ ] && mkdir ~/.local/share/kservices5/
@@ -29,6 +31,7 @@ echo -e "[Desktop Entry] \nType=Service \n#ServiceTypes=application/x-cd-image;m
 Name[ru]=Смонтировать образ \nExec=udisksctl loop-setup -r -f %u \nIcon=media-optical" > ~/.local/share/kservices5/ServiceMenus/mountiso.desktop
 echo -e "[Desktop Entry] \nType=Service \nIcon=system-file-manager \nActions=OpenAsRootKDE5 \nServiceTypes=KonqPopupMenu/Plugin,inode/directory,inode/directory-locked \n \n[Desktop Action OpenAsRootKDE5] \nExec=if [ "$XDG_SESSION_TYPE" = "wayland" ]; then xhost +si:localuser:root && pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY KDE_SESSION_VERSION=5 KDE_FULL_SESSION=true dbus-launch dolphin %U && xhost -si:localuser:root ; else pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY KDE_SESSION_VERSION=5 KDE_FULL_SESSION=true dolphin %U; fi; \nIcon=system-file-manager \nName=Open as Root \nName[ru]=Открыть папку с правами рут \nName[ua]=Відкрити папку з правами рут \nName[zh_CN]=打开具有根权限的文件夹 \nName[zh_TW]=打開具有根許可權的資料夾 \nName[de]=Öffnen des Ordners mit Root-Berechtigungen \nName[ja]=ルート権限を持つフォルダを開く
 Name[ko]=루트 권한이 있는 폴더 열기 \nName[fr]=Ouvrez le dossier avec les privilèges root \nName[el]=Ανοίξτε ως Root \nName[es]=Abrir la carpeta con privilegios de root \nName[tr]=Kök ayrıcalıkları olan klasörü açma \nName[he]=פתח תיקיה עם הרשאות שורש \nName[it]=Aprire la cartella con privilegi radice \nName[ar]=فتح المجلد بامتيازات الجذر \nName[pt_BR]=Abrir pasta com privilégios de root \nName[pt_PT]=Abrir pasta com privilégios de root \nName[sv]=Öppna mapp med root-behörigheter \nName[nb]=Åpne mappen med rotprivilegier" > ~/.local/share/kservices5/ServiceMenus/open_as_root.desktop
+
 
 # config
 kbuildsycoca5
