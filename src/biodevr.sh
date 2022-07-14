62fa373ba3bec98b61d9c54b679a77b5cceacc66
Hang Chen <chen_h@outlook.com>
2022-07-13 21:23:57 -0500

    Update and rename biodev.sh to biodevr.sh

--- src/biodevr.sh
+++ src/biodevr.sh
@@ -11,12 +11,12 @@ TEXT_RESET='\e[0m'
 cd ~/.setup_cache/
 
 
-# ask whether to set up the environment for Bioinformatic developing
-read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to set up the environment for bioinformatic developing, such as R and Python? [y/n/c]'$TEXT_RESET)"$' \n' choice
+# ask whether to set up R environment for Bioinformatic developing
+read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to set up R environment for bioinformatic developing? [y/n/c]'$TEXT_RESET)"$' \n' choice
 case "$choice" in
   y|Y ) # notify start
         sudo echo ""
-        echo -e "${TEXT_YELLOW}Installing bioinfomatics developing enviroment (R/Python)...${TEXT_RESET} \n" && sleep 1
+        echo -e "${TEXT_YELLOW}Installing R bioinfomatics developing enviroment...${TEXT_RESET} \n" && sleep 1
 
         # update system and install packages required by R and R packages installing
         sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install default-jre default-jdk libxml2-dev libssl-dev libcurl4-openssl-dev libnlopt-dev libgeos-dev texlive-latex-extra -y
@@ -36,28 +36,12 @@ case "$choice" in
         Rscript ./rscript/webdriver.R
         
         # install RKWard and fastqc
-        # rkward is dependent on kate, which is installed by Kubuntu by defauly
-        #sudo apt-get install rkward kbibtex fastqc -y
+        # rkward is dependent on kate, which is installed by Kubuntu by default but usually uninstalled by me
+        #sudo apt-get install kate rkward kbibtex fastqc -y
         
-        # install anaconda
-        [ ! -d ./shscript/ ] && mkdir ./shscript/
-        wget -O ./shscript/Anaconda-latest-Linux-x86_64.sh https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh #_to_be_updated
-        echo -e "${TEXT_YELLOW}Please press Space key to scroll the license${TEXT_RESET} \n" && sleep 1
-        bash ./shscript/Anaconda-latest-Linux-x86_64.sh && sleep 3
-        conda update anaconda -y && conda update --all -y
-        conda config --add channels defaults
-        conda config --add channels bioconda
-        conda config --add channels conda-forge
-        #conda install kallisto -y
-        conda config --set auto_activate_base false # disable auto activate base in terminal
-        #conda activate # activate base when needed
-        #rm -rf ~/anaconda3/ # uninstall anaconda
-        # note: conda installed packages are constrained to the conda environment, while pip3 installed packages might affect system environment
-        
-        # install RStudio and Jupyter Lab
+        # install RStudio
         [ ! -d ./devdeb/ ] && mkdir ./devdeb/
         wget -q https://download1.rstudio.org/desktop/jammy/amd64/rstudio-2022.02.3-492-amd64.deb #_to_be_updated
-        wget -q https://github.com/jupyterlab/jupyterlab-desktop/releases/download/v3.3.4-2/JupyterLab-Setup-Debian.deb #_to_be_updated
         sleep 1 && mv -f ./*.deb ./devdeb/ && sudo dpkg -i ./devdeb/*.deb && sleep 1
         sudo apt-get -f -y install
 
@@ -66,13 +50,13 @@ case "$choice" in
         rm -rf ./rscript/ ./devdeb/ ./shscript/
 
         # notify end
-        echo -e " \n${TEXT_GREEN}Bioinformatic developing environment ready!${TEXT_RESET} \n" && sleep 5
+        echo -e " \n${TEXT_GREEN}R bioinformatic developing environment ready!${TEXT_RESET} \n" && sleep 5
 
   n|N ) # notify cancellation
-        echo -e " \n${TEXT_YELLOW}Bioinformatic tools not installed.${TEXT_RESET} \n" && sleep 5;;
+        echo -e " \n${TEXT_YELLOW}R bioinformatic tools not installed.${TEXT_RESET} \n" && sleep 5;;
 
   * ) # notify cancellation
-        echo -e " \n${TEXT_YELLOW}Bioinformatic tools not installed.${TEXT_RESET} \n" && sleep 5;;
+        echo -e " \n${TEXT_YELLOW}R bioinformatic tools not installed.${TEXT_RESET} \n" && sleep 5;;
 
 esac
 
