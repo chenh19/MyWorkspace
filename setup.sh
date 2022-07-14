c840074cc91a89bde5663e97fd0fe25861abdb83
Hang Chen <chen_h@outlook.com>
2022-07-14 00:10:48 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,8 +17,9 @@ cd ~/.setup_cache/
 sudo echo ""
 echo -e "${TEXT_YELLOW}Downloading setup scripts...${TEXT_RESET} \n" && sleep 3
 [ ! -f main ] && wget -q https://codeload.github.com/chenh19/MyWorkspace/zip/refs/heads/main && unzip -o -q main && rm main
+mv -f ./MyWorkspace-main/setup.sh ./
 [ ! -d ./src/ ] && mkdir ./src/
-mv -f ./MyWorkspace-main/setup.sh ./ && mv -f ./MyWorkspace-main/src/* ./src/ 
+mv -f ./MyWorkspace-main/src/* ./src/ 
 [ ! -d ./cfg/ ] && mkdir ./cfg/
 mv -f ./MyWorkspace-main/cfg/* ./cfg/ 
 rm -rf ./MyWorkspace-main/
@@ -26,7 +27,7 @@ echo -e " \n${TEXT_GREEN}All scripts downloaded${TEXT_RESET} \n" && sleep 1
 
 # avoid re-downloading
 sed -i 's+Downloading setup scripts+Continue setting up+g' ~/.setup_cache/setup.sh
-sed -i '19,30d' ~/.setup_cache/setup.sh
+sed -i '19,31d' ~/.setup_cache/setup.sh
 
 # setup
 bash ./src/deb.sh
