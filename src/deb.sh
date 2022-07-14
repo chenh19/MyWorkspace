6225c84f31c878cea44007c30a4521aa42e8ddf4
Hang Chen <chen_h@outlook.com>
2022-07-14 01:23:01 -0500

    Update deb.sh

--- src/deb.sh
+++ src/deb.sh
@@ -34,7 +34,7 @@ sudo apt-get install kwrite krita seahorse evolution evolution-ews xdotool kdock
 
 # install apps (downloaded)
   [ ! -d ./deb/ ] && mkdir ./deb/
-  echo -e "${TEXT_YELLOW}Downloading deb packages...${TEXT_RESET} \n" && sleep 1
+  echo -e " \n${TEXT_YELLOW}Downloading deb packages...${TEXT_RESET} \n" && sleep 1
   
   # redirecting links
     wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
