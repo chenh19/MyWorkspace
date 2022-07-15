e6f6bf8eec2f16e7f3e717d1261bc1ea551ccb5b
Hang Chen <chen_h@outlook.com>
2022-07-15 17:21:37 -0500

    Update and rename cfgapp.sh to sysapp.sh

--- src/sysapp.sh
+++ src/sysapp.sh
@@ -188,43 +188,7 @@ bash <(wget -qO- https://raw.githubusercontent.com/chenh19/alt_rm/main/install.s
 cp -f ./cfg/touchegg/* ~/.config/touchegg/
 
 ######################################################################################
-# aske whether to configure apps manually
-######################################################################################
 
-# fdm
-echo -e " \n${TEXT_YELLOW}Please config and then close Free Donwload Manager to continue.${TEXT_RESET} \n" && sleep 1
-/opt/freedownloadmanager/fdm
-
-######################################################################################
-
-# Inkscape
-echo -e " \n${TEXT_YELLOW}Please config and then close Inkscape to continue.${TEXT_RESET} \n" && sleep 1
-inkscape
-
-######################################################################################
-
-# qView
-# do not check update notification when first open 
-echo -e " \n${TEXT_YELLOW}Please close qViw to continue.${TEXT_RESET} \n" && sleep 1
-qview
-
-######################################################################################
-
-# libreoffice
-echo -e " \n${TEXT_YELLOW}Please config ${TEXT_GREEN}[themes/fonts/saving formats/toolbars]${TEXT_YELLOW} and then close LibreOffice to continue.${TEXT_RESET} \n" && sleep 1
-libreoffice
-
-######################################################################################
-
-# chrome
-echo -e " \n${TEXT_YELLOW}Please login to your Google account and then close Chrome to continue.${TEXT_RESET} \n" && sleep 1
-/usr/bin/google-chrome-stable
-
-######################################################################################
-
-# evolution
-
-######################################################################################
 
 # notify end
 echo -e " \n${TEXT_GREEN}All apps and widgets configured!${TEXT_RESET} \n" && sleep 5
