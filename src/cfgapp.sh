1c45f25f224366957675a4470a6fcc8c078811bf
Hang Chen <chen_h@outlook.com>
2022-07-15 17:13:20 -0500

    Update cfgapp.sh

--- src/cfgapp.sh
+++ src/cfgapp.sh
@@ -189,9 +189,22 @@ cp -f ./cfg/touchegg/* ~/.config/touchegg/
 
 ######################################################################################
 
+# fdm
+echo -e " \n${TEXT_YELLOW}Please config and then close Free Donwload Manager to continue.${TEXT_RESET} \n" && sleep 1
+/opt/freedownloadmanager/fdm
+
+######################################################################################
+
 # Inkscape
 echo -e " \n${TEXT_YELLOW}Please config and then close Inkscape to continue.${TEXT_RESET} \n" && sleep 1
 inkscape
+
+######################################################################################
+
+# qView
+# do not check update notification when first open 
+echo -e " \n${TEXT_YELLOW}Please close qViw to continue.${TEXT_RESET} \n" && sleep 1
+qview
 
 ######################################################################################
 
