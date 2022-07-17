b38c4fe5b792c42c7d5f9263382c74517cc9acd4
Hang Chen <chen_h@outlook.com>
2022-07-17 16:59:22 -0500

    Update usrapp.sh

--- src/cfg/usrapp.sh
+++ src/cfg/usrapp.sh
@@ -1,69 +1,13 @@
+#!/bin/bash
+# This script helps maunal configuration
+
+# set terminal font color
+TEXT_YELLOW='\e[1;33m'
+TEXT_GREEN='\e[1;32m'
+TEXT_RESET='\e[0m'
+
+# set working directory
+[ ! -d ~/.setup_cache/ ] && mkdir ~/.setup_cache/
+cd ~/.setup_cache/
+
 # aske whether to configure apps manually
-######################################################################################
-
-# fdm
-echo -e " \n${TEXT_YELLOW}Please config and then close Free Donwload Manager to continue.${TEXT_RESET} \n" && sleep 1
-/opt/freedownloadmanager/fdm
-
-######################################################################################
-
-# Inkscape
-echo -e " \n${TEXT_YELLOW}Please config and then close Inkscape to continue.${TEXT_RESET} \n" && sleep 1
-inkscape
-
-######################################################################################
-
-# qView
-# do not check update notification when first open 
-echo -e " \n${TEXT_YELLOW}Please close qViw to continue.${TEXT_RESET} \n" && sleep 1
-qview
-
-######################################################################################
-
-# libreoffice
-echo -e " \n${TEXT_YELLOW}Please config ${TEXT_GREEN}[themes/fonts/saving formats/toolbars]${TEXT_YELLOW} and then close LibreOffice to continue.${TEXT_RESET} \n" && sleep 1
-libreoffice
-
-######################################################################################
-
-# chrome
-echo -e " \n${TEXT_YELLOW}Please login to your Google account and then close Chrome to continue.${TEXT_RESET} \n" && sleep 1
-/usr/bin/google-chrome-stable
-
-######################################################################################
-
-# evolution
-# restore backup
-# ask whether set evolution as autostart
-
-######################################################################################
-
-# expandrive
-
-~/.config/autostart/expandrive --autorun.desktop
-
-[Desktop Entry]
-Type=Application
-Version=1.0
-Name=expandrive --autorun
-Comment=expandrive --autorunstartup script
-Exec=/opt/ExpanDrive/expandrive --autorun
-StartupNotify=false
-Terminal=false
-
-# ask whether set evolution as autostart
-
-######################################################################################
-
-# slack
-#config
-/usr/bin/slack
-
-# ask whether to set as autostart
-
-######################################################################################
-
-# eudic
-/opt/eudic/eudic.AppImage
-echo -e " \n${TEXT_YELLOW}Please config and then close EuDic to continue.${TEXT_RESET} \n" && sleep 1
-XDG_CURRENT_DESKTOP=GNOME /opt/eudic/eudic.AppImage
