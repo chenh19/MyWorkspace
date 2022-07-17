9fa5206045ce91912584cf3337908a7e6ecc0b86
Hang Chen <chen_h@outlook.com>
2022-07-17 16:03:50 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,10 +34,10 @@ cp -rf ./MyWorkspace-main/src/cfg/* ./cfg/
 rm -rf ./MyWorkspace-main/
 echo -e "${TEXT_GREEN}All scripts downloaded${TEXT_RESET} \n" && sleep 1
 
-# avoid re-downloading
-sed -i 's+Downloading setup scripts+Continue setting up+g' ~/.setup_cache/setup.sh
-sed -i '27,39d' ~/.setup_cache/setup.sh
-sed -i '39,43d' ~/.setup_cache/inst/wechat.sh
+# modify setup scripts
+sed -i '39,43d' ~/.setup_cache/inst/wechat.sh # save wechat sacling config for later
+sed -i 's+Downloading setup scripts+Continue setting up+g' ~/.setup_cache/setup.sh # avoid re-downloading
+sed -i '27,40d' ~/.setup_cache/setup.sh # avoid re-downloading
 
 # setup
 bash ./inst/deb.sh
