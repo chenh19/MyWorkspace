72fec52cb1e745328eb6c4f6c42a43d1b27adda6
Hang Chen <chen_h@outlook.com>
2022-07-17 15:38:24 -0500

    Update appimage.sh

--- src/inst/appimage.sh
+++ src/inst/appimage.sh
@@ -52,9 +52,7 @@ sudo cp -rf ./*.desktop /usr/share/applications/ && sleep 5
 # Etcher
 [ ! -d ~/.config/balena-etcher-electron/ ] && mkdir ~/.config/balena-etcher-electron/
 echo -e '{ \n  "errorReporting": false, \n  "updatesEnabled": true, \n  "desktopNotifications": true, \n  "autoBlockmapping": true, \n  "decompressFirst": true \n}' > ~/.config/balena-etcher-electron/config.json
-# Eudic
-echo -e " \n${TEXT_YELLOW}Please config and then close EuDic to continue.${TEXT_RESET} \n" && sleep 1
-XDG_CURRENT_DESKTOP=GNOME /opt/eudic/eudic.AppImage
+
 
 # cleanup
 rm -rf ./etcher/ ./tropy/ ./eudic/ && rm ./*.desktop
