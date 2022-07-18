7d15c73d133fdb172b1af662a8d13ff7d655d342
Hang Chen <chen_h@outlook.com>
2022-07-17 23:11:24 -0500

    Update 0_deb.sh

--- src/inst/0_deb.sh
+++ src/inst/0_deb.sh
@@ -203,7 +203,7 @@ case "$choice" in
         echo -e " \n${TEXT_GREEN}Apt installed apps Configured!${TEXT_RESET} \n" && sleep 5;;
         
   * )   # notify cancellation
-        echo -e " \n${TEXT_YELLOW}Apt installed apps Not configured.${TEXT_RESET} \n" && sleep 5;;
+        echo -e " \n${TEXT_YELLOW}Apt installed apps not configured.${TEXT_RESET} \n" && sleep 5;;
         
 esac
 ###>>>sed-i-d-end-1
