d29edf5a0ad45bf3703af4eeb4fb3b1c7c1621ae
Hang Chen <chen_h@outlook.com>
2022-07-18 04:19:14 -0500

    Update 5_usrapp_tail.sh

--- src/cfg/5_usrapp_tail.sh
+++ src/cfg/5_usrapp_tail.sh
@@ -6,3 +6,6 @@
         echo -e " \n${TEXT_YELLOW}Apps not configured.${TEXT_RESET} \n" && sleep 5;;
         
 esac
+
+# mark setup.sh
+sed -i 's+bash ./cfg/5_usrapp.sh+#bash ./cfg/5_usrapp.sh+g' ~/.setup_cache/setup.sh
