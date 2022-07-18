7ec6de221cc8491f70fdcf1701d6d82496b82237
Hang Chen <chen_h@outlook.com>
2022-07-18 03:57:18 -0500

    Update 9_biodevpy.sh

--- src/inst/9_biodevpy.sh
+++ src/inst/9_biodevpy.sh
@@ -27,7 +27,7 @@ case "$choice" in
         
         # config conda
         echo -e " \n${TEXT_YELLOW}Paste below command in the new terminal window and execute. Once finished, please close the new terminal window to continue.${TEXT_RESET} \n" && sleep 1
-        echo -e " \n${TEXT_GREEN}bash <(wget -qO- https://raw.githubusercontent.com/chenh19/MyWorkspace/main/src/inst/9_biodevpy_conda.sh)${TEXT_RESET} \n" && sleep 1
+        echo -e " \n${TEXT_GREEN}bash <(wget -qO- https://raw.githubusercontent.com/chenh19/MyWorkspace/main/src/cfg/conda/conda.sh)${TEXT_RESET} \n" && sleep 1
         konsole
         
         # ask whether to install Jupyter Lab
