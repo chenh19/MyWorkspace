a78af7f153b11c5c47fdd5fa723d75400396f3db
Hang Chen <chen_h@outlook.com>
2022-07-17 19:38:50 -0500

    Update usrapp.sh

--- src/cfg/usrapp.sh
+++ src/cfg/usrapp.sh
@@ -11,3 +11,10 @@ TEXT_RESET='\e[0m'
 cd ~/.setup_cache/
 
 # aske whether to configure apps manually
+sudo echo ""
+read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to configure apps manually? [y/n/c]'$TEXT_RESET)"$' \n' choice
+case "$choice" in
+  y|Y ) # notify start
+        sudo echo ""
+        echo -e "${TEXT_YELLOW}Installing RStudio...${TEXT_RESET} \n" && sleep 1
+
