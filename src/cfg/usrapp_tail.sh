0a65c32d5a09d6f2206a897d2e8c91c9f8e1c56c
Hang Chen <chen_h@outlook.com>
2022-07-17 23:51:06 -0500

    Create usrapp_tail.sh

---
+++ src/cfg/usrapp_tail.sh
@@ -0,0 +1,7 @@
+        # notify end
+        echo -e " \n${TEXT_GREEN}All apps Configured!${TEXT_RESET} \n" && sleep 5;;
+        
+  * )   # notify cancellation
+        echo -e " \n${TEXT_YELLOW}Apps not configured.${TEXT_RESET} \n" && sleep 5;;
+        
+esac
