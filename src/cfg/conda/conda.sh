bf0b0809e372ecb652295eb01c6f7463d87cb0b7
Hang Chen <chen_h@outlook.com>
2022-07-19 21:11:47 -0500

    Update conda.sh

--- src/cfg/conda/conda.sh
+++ src/cfg/conda/conda.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
 # config conda in a new terminal window
-conda update conda && conda update anaconda -y && conda update --all -y
-conda config --add channels conda-forge
+conda config --set auto_activate_base false
+#conda config --add channels conda-forge
 conda config --add channels bioconda
 conda config --add channels defaults
 
 # note: conda installed packages are constrained to the conda environment, while pip3 installed packages might affect system environment
+conda update --all -y
 conda install -c anaconda numpy pandas -y
-#conda install kallisto -y
-conda install -c conda-forge mamba
-mamba create -c conda-forge -c bioconda -n snakemake snakemake
+conda install -c bioconda snakemake kallisto bustools -y
 
-# disable auto activate base in terminal
-conda config --set auto_activate_base false
 #conda activate # activate base when needed
 #conda activate snakemake # activate base when needed
-
 # note: if conda stucks at solving environment
 #conda config --remove channels conda-forge
 #conda config --add channels conda-forge
 #conda update conda
-
 # uninstall anaconda
 #rm -rf ~/anaconda3/
