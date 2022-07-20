1a953e8b5d415792d8eac9132040509ece43e6a4
Hang Chen <chen_h@outlook.com>
2022-07-19 20:07:08 -0500

    Update 9_biodevpy.sh

--- src/inst/9_biodevpy.sh
+++ src/inst/9_biodevpy.sh
@@ -26,7 +26,7 @@ case "$choice" in
         echo -e "${TEXT_YELLOW}Please press Space key to scroll the license${TEXT_RESET} \n" && sleep 1
         bash ./shscript/Anaconda-latest-Linux-x86_64.sh && sleep 3
         
-        # config conda
+        # conda config and install
         echo -e " \n${TEXT_YELLOW}Paste below command in the new terminal window and execute. Once finished, please close the new terminal window to continue.${TEXT_RESET} \n" && sleep 1
         echo -e " \n${TEXT_GREEN}bash <(wget -qO- https://raw.githubusercontent.com/chenh19/MyWorkspace/main/src/cfg/conda/conda.sh)${TEXT_RESET} \n" && sleep 1
         konsole
