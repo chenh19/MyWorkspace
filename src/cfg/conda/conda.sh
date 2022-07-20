c70860924c37955764265073c398ca4fecca26c5
Hang Chen <chen_h@outlook.com>
2022-07-19 20:25:55 -0500

    Update conda.sh

--- src/cfg/conda/conda.sh
+++ src/cfg/conda/conda.sh
@@ -5,12 +5,22 @@ conda update anaconda -y && conda update --all -y
 conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
+
+# note: conda installed packages are constrained to the conda environment, while pip3 installed packages might affect system environment
 conda install -c anaconda numpy pandas -y
 #conda install kallisto -y
 conda install -n base -c conda-forge mamba
 mamba create -c conda-forge -c bioconda -n snakemake snakemake
-conda config --set auto_activate_base false # disable auto activate base in terminal
+
+# disable auto activate base in terminal
+conda config --set auto_activate_base false
 #conda activate # activate base when needed
 #conda activate snakemake # activate base when needed
-#rm -rf ~/anaconda3/ # uninstall anaconda
-# note: conda installed packages are constrained to the conda environment, while pip3 installed packages might affect system environment
+
+# note: if conda stucks at solving environment
+#conda config --remove channels conda-forge
+#conda config --add channels conda-forge
+#conda update conda
+
+# uninstall anaconda
+#rm -rf ~/anaconda3/
