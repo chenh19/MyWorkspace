c26c20c1f1bd0a74ff7f03b7fec0a0e87c3a0f1c
Hang Chen <chen_h@outlook.com>
2022-07-19 20:05:23 -0500

    Update conda.sh

--- src/cfg/conda/conda.sh
+++ src/cfg/conda/conda.sh
@@ -8,6 +8,9 @@ conda config --add channels conda-forge
 conda install -c anaconda numpy pandas -y
 #conda install kallisto -y
 conda config --set auto_activate_base false # disable auto activate base in terminal
+conda install -n base -c conda-forge mamba
+mamba create -c conda-forge -c bioconda -n snakemake snakemake
 #conda activate # activate base when needed
+#conda activate snakemake # activate base when needed
 #rm -rf ~/anaconda3/ # uninstall anaconda
 # note: conda installed packages are constrained to the conda environment, while pip3 installed packages might affect system environment
