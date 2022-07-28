fdb46711a3f4f3760f0f91163b26d999541d9e98
Hang Chen <chen_h@outlook.com>
2022-07-27 22:51:07 -0500

    Update 9_biodevpy.sh

--- src/inst/9_biodevpy.sh
+++ src/inst/9_biodevpy.sh
@@ -22,8 +22,8 @@ case "$choice" in
         
         # install anaconda
         [ ! -d ./shscript/ ] && mkdir ./shscript/
-        wget -O ./shscript/Anaconda-latest-Linux-x86_64.sh https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh #_to_be_updated
-        echo -e "${TEXT_YELLOW}Please press Space key to scroll the license${TEXT_RESET} \n" && sleep 1
+        wget -O ./shscript/Anaconda-latest-Linux-x86_64.sh https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh && echo '"Anaconda" installing package is downloaded.' && sleep 1 #_to_be_updated
+        echo -e "${TEXT_YELLOW}Please press ${TEXT_GREEN}[Enter]${TEXT_YELLOW} and then ${TEXT_GREEN}[Space]${TEXT_YELLOW} key to scroll the license${TEXT_RESET} \n" && sleep 1
         bash ./shscript/Anaconda-latest-Linux-x86_64.sh && sleep 3
         
         # conda config and install
@@ -41,8 +41,8 @@ case "$choice" in
                 
                 # install Jupyter Lab
                 [ ! -d ./devdeb/ ] && mkdir ./devdeb/
-                wget -q https://github.com/jupyterlab/jupyterlab-desktop/releases/download/v3.3.4-2/JupyterLab-Setup-Debian.deb #_to_be_updated
-                sleep 1 && mv -f ./*.deb ./devdeb/ && sudo dpkg -i ./devdeb/*.deb && sleep 1
+                wget -q https://github.com/jupyterlab/jupyterlab-desktop/releases/download/v3.3.4-2/JupyterLab-Setup-Debian.deb && echo '"JupyterLab" deb package is downloaded.' && sleep 1 #_to_be_updated
+                mv -f ./*.deb ./devdeb/ && sudo dpkg -i ./devdeb/*.deb && sleep 1
                 sudo apt-get -f -y install
                 
                 # config Jupyter Lab
