92896259c9f60ddf6b0b0439e22a709f94fe8a66
Hang Chen <chen_h@outlook.com>
2022-07-29 14:56:36 -0500

    Update finish.sh

--- src/cfg/finish/finish.sh
+++ src/cfg/finish/finish.sh
@@ -12,14 +12,13 @@ echo -e "## Personalization: \n- [ ] Right click on desktop > Configure Desktop 
 sleep 1
 chmod +x ~/Desktop/*.desktop
 sleep 1
-kwriteconfig5 --file ~/.config/konsolerc --group MainWindow --key 'State' "AAAA/wAAAAD9AAAAAQAAAAAAAAAAAAAAAPwCAAAAAfsAAAAcAFMAUwBIAE0AYQBuAGEAZwBlAHIARABvAGMAawAAAAAA/////wAAANUBAAADAAADIwAAAiUAAAAEAAAABAAAAAgAAAAI/AAAAAEAAAACAAAAAgAAABYAbQBhAGkAbgBUAG8AbwBsAEIAYQByAQAAAAD/////AAAAAAAAAAAAAAAcAHMAZQBzAHMAaQBvAG4AVABvAG8AbABiAGEAcgEAAADo/////wAAAAAAAAAA"
-sleep 1
 cat /proc/bus/input/devices | grep -i syna > ~/.config/touchpadxlibinputrc
 sed -i 's+N: Name="+[+g' ~/.config/touchpadxlibinputrc
 sed -i 's+"+]+g' ~/.config/touchpadxlibinputrc
 sed -i '2,200d' ~/.config/touchpadxlibinputrc
 echo -e "clickMethodAreas=false \nclickMethodClickfinger=true \nnaturalScroll=true \npointerAcceleration=0.6 \ntapToClick=true" | tee -a ~/.config/touchpadxlibinputrc
 sleep 1
+kwriteconfig5 --file ~/.config/konsolerc --group MainWindow --key 'State' "AAAA/wAAAAD9AAAAAQAAAAAAAAAAAAAAAPwCAAAAAfsAAAAcAFMAUwBIAE0AYQBuAGEAZwBlAHIARABvAGMAawAAAAAA/////wAAANUBAAADAAADIwAAAiUAAAAEAAAABAAAAAgAAAAI/AAAAAEAAAACAAAAAgAAABYAbQBhAGkAbgBUAG8AbwBsAEIAYQByAQAAAAD/////AAAAAAAAAAAAAAAcAHMAZQBzAHMAaQBvAG4AVABvAG8AbABiAGEAcgEAAADo/////wAAAAAAAAAA"
 kwriteconfig5 --file ~/.config/systemmonitorrc --group General --key 'hiddenPages' "applications.page"
 kwriteconfig5 --file ~/.config/systemmonitorrc --group General --key 'pageOrder' "overview.page,process.page,history.page,application.page"
 sleep 1
