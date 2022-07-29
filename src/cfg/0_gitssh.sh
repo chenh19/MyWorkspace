085d60809ff54643662bc7c6fef0fbfc89e120fa
Hang Chen <chen_h@outlook.com>
2022-07-29 15:26:12 -0500

    Update 0_gitssh.sh

--- src/cfg/0_gitssh.sh
+++ src/cfg/0_gitssh.sh
@@ -11,31 +11,40 @@ TEXT_RESET='\e[0m'
 cd ~
 
 
-# notify start
+# ask whether to set up an ssh key for github
 sudo echo ""
-echo -e "${TEXT_YELLOW}Setting Git SSH...${TEXT_RESET} \n" && sleep 1
-
-# check for existing keys
-#ls -al ~/.ssh
-
-# ask for your_email
-read -p "$(echo -e $TEXT_YELLOW'Please enter your GitHub email address: '$TEXT_RESET)"$' \n' email
-
-# create a key if does not exist
-echo -e " \n${TEXT_YELLOW}When asked "Enter a file in which to save the key", please press [Enter] (default file location)${TEXT_RESET} \n"
-echo -e " \n${TEXT_YELLOW}Then, please input a passphrase (anything you can remember).${TEXT_RESET} \n"
-ssh-keygen -t ed25519 -C $email
-
-# add SSH key to ssh-agent-
-eval `ssh-agent -s`
-ssh-add ~/.ssh/id_ed25519
-
-# get the key
-echo -e " \n${TEXT_GREEN}All done! Git SSH key: ${TEXT_RESET} \n"
-cat ~/.ssh/id_ed25519.pub
-cat ~/.ssh/id_ed25519.pub > ~/Documents/gitssh.txt
-echo -e " \n${TEXT_GREEN}You may copy the above SSH key and add it to your GitHub. Or, it's also save in ~/Documents/gitssh.txt file and you may work on it later.${TEXT_RESET} \n"
-
+read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to set up an SSH key for GitHub? [y/n/c]'$TEXT_RESET)"$' \n' choice
+case "$choice" in
+  y|Y ) # notify start
+        sudo echo ""
+        echo -e "${TEXT_YELLOW}Setting up Git SSH key...${TEXT_RESET} \n" && sleep 1
+        
+        # check for existing keys
+        #ls -al ~/.ssh
+        
+        # ask for your_email
+        read -p "$(echo -e $TEXT_YELLOW'Please enter your GitHub email address: '$TEXT_RESET)"$' \n' email
+        
+        # create a key if does not exist
+        echo -e " \n${TEXT_YELLOW}When asked "Enter a file in which to save the key", please press [Enter] (default file location)${TEXT_RESET} \n"
+        echo -e " \n${TEXT_YELLOW}Then, please input a passphrase (anything you can remember).${TEXT_RESET} \n"
+        ssh-keygen -t ed25519 -C $email
+        
+        # add SSH key to ssh-agent-
+        eval `ssh-agent -s`
+        ssh-add ~/.ssh/id_ed25519
+        
+        # get the key
+        echo -e " \n${TEXT_GREEN}All done! Git SSH key: ${TEXT_RESET} \n"
+        cat ~/.ssh/id_ed25519.pub
+        cat ~/.ssh/id_ed25519.pub > ~/Documents/gitssh.txt
+        
+        # notify end
+        echo -e " \n${TEXT_GREEN}You may copy the above key and add it to your GitHub. Or, it's also save in ~/Documents/gitssh.txt file, and you may work on it later.${TEXT_RESET} \n" && sleep 5;;
+        
+  * ) # notify cancellation
+        echo -e " \n${TEXT_YELLOW}Git SSH not configured.${TEXT_RESET} \n" && sleep 5;;
+esac
 
 # mark setup.sh
 sed -i 's+bash ./cfg/0_gitssh.sh+#bash ./cfg/0_gitssh.sh+g' ~/.setup_cache/setup.sh
