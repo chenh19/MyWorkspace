2ee4b195004af584aaa499e37114501c18451a2f
Hang Chen <chen_h@outlook.com>
2022-10-04 12:21:00 -0500

    Update apt_warning.sh

--- src/cfg/apt/apt_warning.sh
+++ src/cfg/apt/apt_warning.sh
@@ -4,7 +4,6 @@
 # set variables
 resilio_id=
 fdm_id=
-slack_id=
 
 # resilio
 sudo apt-key export $resilio_id | sudo gpg --dearmour -o /usr/share/keyrings/pgdg_resilio.gpg
@@ -16,15 +15,9 @@ sudo apt-key export $fdm_id | sudo gpg --dearmour -o /usr/share/keyrings/pgdg_fd
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/pgdg_fdm.gpg] https://debrepo.freedownloadmanager.org/ bionic main" | sudo tee /etc/apt/sources.list.d/freedownloadmanager.list
 sudo apt-key del $fdm_id
 
-# slack
-sudo apt-key export $slack_id | sudo gpg --dearmour -o /usr/share/keyrings/pgdg_slack.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/pgdg_slack.gpg] https://packagecloud.io/slacktechnologies/slack/debian/ jessie main" | sudo tee /etc/apt/sources.list.d/slack.list
-sudo apt-key del $slack_id
-
 # unset variables
 unset resilio_id
 unset fdm_id
-unset slack_id
 
 
 
