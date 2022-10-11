4c16b19ce059bd7913ac4a4689d7bf5a162cd72e
Hang Chen <chen_h@outlook.com>
2022-10-11 14:57:00 -0500

    Update 3_sysapp.sh

--- src/cfg/3_sysapp.sh
+++ src/cfg/3_sysapp.sh
@@ -176,9 +176,10 @@ sudo cp -rf ./fonts/windows/ /usr/share/fonts/
 
 ######################################################################################
 
-# sysupdate
+# command alias
+## sysupdate
 bash <(wget -qO- https://raw.githubusercontent.com/chenh19/sysupdate/main/install.sh)
-# alt_rm
+## alt_rm
 bash <(wget -qO- https://raw.githubusercontent.com/chenh19/alt_rm/main/install.sh)
 
 ######################################################################################
