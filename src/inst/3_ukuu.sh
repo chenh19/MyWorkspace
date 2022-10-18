4018c8e304c1a0f0d8124178125532a1fed8e306
Hang Chen <chen_h@outlook.com>
2022-10-18 11:31:10 -0500

    Update 3_ukuu.sh

--- src/inst/3_ukuu.sh
+++ src/inst/3_ukuu.sh
@@ -18,10 +18,6 @@ echo -e "${TEXT_YELLOW}Installing UKUU...${TEXT_RESET} \n" && sleep 1
 # download and install UKUU
 wget -O - https://teejeetech.com/install-ukuu-8ALv9hCkUG.sh | bash
 
-# write license key
-[ ! -d ~/.config/ukuu/ ] && mkdir ~/.config/ukuu/
-echo -e "SGFuZyBDaGVufGNoZW5faEBvdXRsb29rLmNvbXwzUE9NLURMWkEtVFdIRy1ITzgwLUM2Nlg=" > ~/.config/ukuu/license.dat
-
 # install latest kernel
 sudo ukuu --scripted --install-latest
 
