25ee56ae81393b23200d1603b72490d04c724667
Hang Chen <chen_h@outlook.com>
2022-10-19 16:30:34 -0500

    Update 8_biodevr.sh

--- src/inst/8_biodevr.sh
+++ src/inst/8_biodevr.sh
@@ -19,6 +19,7 @@ case "$choice" in
         sudo echo ""
         echo -e "${TEXT_YELLOW}Installing R bioinfomatics developing enviroment (this might take a while)...${TEXT_RESET} \n" && sleep 1
         sudo apt-get update && sudo apt-get upgrade -y
+        [ ! -d ~/Developing/ ] && mkdir ~/Developing/ && kwriteconfig5 --file ~/Developing/.directory --group "Desktop Entry" --key Icon "folder-script"
 
         # install packages required by R and R packages installing
         sudo apt-get install default-jre default-jdk curl openssl libxml2-dev libssl-dev libcurl4-openssl-dev libnlopt-dev libgeos-dev texlive-latex-extra -y
