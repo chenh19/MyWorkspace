77ee3bab89b1670b025b7da500e7d5e6b0c1eae7
Hang Chen <chen_h@outlook.com>
2023-02-09 12:52:23 -0600

    Update 5_wechat.sh

--- src/inst/5_wechat.sh
+++ src/inst/5_wechat.sh
@@ -18,36 +18,28 @@ case "$choice" in
   y|Y ) # notify start
         sudo echo ""
         echo -e "${TEXT_YELLOW}Installing WeChat...${TEXT_RESET} \n" && sleep 1
+        sudo apt-get update && sudo apt-get install fonts-wqy-microhei fonts-wqy-zenhei xfonts-wqy libjpeg62:i386 -y
         
         # install deepin wine
         wget -O- https://deepin-wine.i-m.dev/setup.sh | sh
         
         # install weixin
-        sudo apt-get install com.qq.weixin.deepin -y
+        echo "" && sudo apt-get install com.qq.weixin.deepin -y
         
-        # install dependencies
-        #sudo apt-get update && sudo apt-get install fonts-wqy-microhei fonts-wqy-zenhei xfonts-wqy libjpeg62:i386 -y
-
-        # install deepin-wine
-        #wget -q https://www.dropbox.com/s/t43dz4ws6576r58/deepin-wine-ubuntu-master.zip?dl=0 && echo '"Deepin-wine WeChat" packages downloaded.' && sleep 5 #_to_be_updated
-        #unzip -o -q deepin-wine-ubuntu-master.zip?dl=0 && sleep 1 && rm -f deepin-wine-ubuntu-master.zip?dl=0 && sleep 1
-        #sudo bash ./deepin-wine-ubuntu-master/deepin-wine-setup.sh
-        #sudo bash ./deepin-wine-ubuntu-master/KDE-install.sh
-        #sudo ln -sf /usr/lib/i386-linux-gnu/deepin-wine/libwine.so.1.0 /usr/lib/i386-linux-gnu/deepin-wine/libwine.so.1
-        
-        # configure locale
-        #echo -e " \n${TEXT_YELLOW}Please use [Space] to select ${TEXT_GREEN}zh_CN.UTF-8${TEXT_YELLOW} and then [Enter] to continue.${TEXT_RESET} \n" && sleep 5
-        #sudo dpkg-reconfigure locales
-        #sudo locale-gen zh_CN.UTF-8 #might work
-        #sudo localectl set-locale LANG=en_US.UTF-8 #doesn't work
-        #sudo update-locale LANG=zh_CN.UTF-8 #doesn't work
-
-        # install wechat
-        #sudo dpkg -i ./deepin-wine-ubuntu-master/deepin.com.wechat_2.6.8.65deepin0_i386.deb
-
         # configure language
-        #sudo sed -i 's+WINE_CMD="deepin-wine"+WINE_CMD="LC_ALL=zh_CN.UTF-8 deepin-wine"+g' /opt/deepinwine/tools/run_v2.sh
-        #"/opt/deepinwine/apps/Deepin-WeChat/run.sh" -u %u && sleep 10 && killall -9 WeChat.exe && sleep 5
+        echo -e " \n${TEXT_YELLOW}Please use [Space] to select ${TEXT_GREEN}zh_CN.UTF-8${TEXT_YELLOW} and then [Enter] to continue.${TEXT_RESET} \n" && sleep 5
+        sudo dpkg-reconfigure locales
+        [ -f /opt/deepinwine/tools/run.sh ] && sudo sed -i 's+WINE_CMD="deepin-wine"+WINE_CMD="LC_ALL=zh_CN.UTF-8 deepin-wine"+g' /opt/deepinwine/tools/run.sh
+        [ -f /opt/deepinwine/tools/run_v2.sh ] && sudo sed -i 's+WINE_CMD="deepin-wine"+WINE_CMD="LC_ALL=zh_CN.UTF-8 deepin-wine"+g' /opt/deepinwine/tools/run_v2.sh
+        [ -f /opt/deepinwine/tools/run_v3.sh ] && sudo sed -i 's+WINE_CMD="deepin-wine"+WINE_CMD="LC_ALL=zh_CN.UTF-8 deepin-wine"+g' /opt/deepinwine/tools/run_v3.sh
+        [ -f /opt/deepinwine/tools/run_v4.sh ] && sudo sed -i 's+WINE_CMD="deepin-wine"+WINE_CMD="LC_ALL=zh_CN.UTF-8 deepin-wine"+g' /opt/deepinwine/tools/run_v4.sh
+        [ -f /opt/deepinwine/tools/run_v5.sh ] && sudo sed -i 's+WINE_CMD="deepin-wine"+WINE_CMD="LC_ALL=zh_CN.UTF-8 deepin-wine"+g' /opt/deepinwine/tools/run_v5.sh
+
+        # create shortcut
+        sudo cp -f /opt/shortcut/com.qq.weixin.deepin.desktop /usr/share/applications/
+
+        # run wechat for once
+        "/opt/apps/com.qq.weixin.deepin/files/run.sh" -f %f && sleep 10 && killall -9 WeChat.exe && sleep 5
 
         # cleanup
         #rm -rf ./deepin-wine-ubuntu-master/
