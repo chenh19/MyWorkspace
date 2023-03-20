a32f17a07e1b7d0919349efe95d66a1314a76915
Hang Chen <chen_h@outlook.com>
2023-03-20 12:38:43 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ done
 echo ""
 
 ## prepare all scripts
-[ ! -f main ] && wget -q https://codeload.github.com/chenh19/MyWorkspace/zip/refs/heads/main && sleep 1
+[ ! -f main ] && wget -q https://github.com/chenh19/MyWorkspace/archive/refs/heads/main.zip && sleep 1
 unzip -o -q main.zip && sleep 1 && rm main.zip
 cp -rf ./MyWorkspace-main/setup.sh ./
 [ ! -d ./inst/ ] && mkdir ./inst/
