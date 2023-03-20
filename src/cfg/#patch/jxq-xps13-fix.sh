93fe6f82f3aa9c65fce6fa376b4ea709de33459c
Hang Chen <chen_h@outlook.com>
2023-03-20 13:18:43 -0500

    Update jxq-xps13-fix.sh

--- src/cfg/#patch/jxq-xps13-fix.sh
+++ src/cfg/#patch/jxq-xps13-fix.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# bash <(wget -qO- https://raw.githubusercontent.com/chenh19/MyWorkspace/main/src/cfg/%23patch/jxq-xps13-fix.sh)
 
 # remove deepin-wine apt source
 [ -f /etc/apt/sources.list.d/deepin-wine.i-m.dev.list ] && sudo rm -f /etc/apt/sources.list.d/deepin-wine.i-m.dev.list
@@ -7,8 +8,8 @@ if grep -q "deepin-wine" /etc/apt/sources.list ; then sudo sed -i '/deepin-wine/
 sudo apt-get update && sudo apt-get autoremove -y && sudo apt-get clean
 
 # icons
-[ ! -f main ] && wget -q https://github.com/chenh19/MyWorkspace/archive/refs/heads/main.zip && sleep 1
-unzip -o -q main && sleep 1 && rm main
+[ ! -f main.zip ] && wget -q https://github.com/chenh19/MyWorkspace/archive/refs/heads/main.zip && sleep 1
+unzip -o -q main.zip && sleep 1 && rm main.zip
 sudo cp -rf ./MyWorkspace-main/src/cfg/icon/ /opt/ && sleep 1 && rm -rf ./MyWorkspace-main/
 
 # sysupdate
