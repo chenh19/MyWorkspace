2617333d6a387f11fa169dbea839cf1a67317110
Hang Chen <chen_h@outlook.com>
2023-03-20 13:20:37 -0500

    Update wsy-xps13-fix.sh

--- src/cfg/#patch/wsy-xps13-fix.sh
+++ src/cfg/#patch/wsy-xps13-fix.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# bash <(wget -qO- https://raw.githubusercontent.com/chenh19/MyWorkspace/main/src/cfg/%23patch/wsy-xps13-fix.sh)
 
 # remove deepin-wine apt source
 [ -f /etc/apt/sources.list.d/deepin-wine.i-m.dev.list ] && sudo rm -f /etc/apt/sources.list.d/deepin-wine.i-m.dev.list
@@ -10,8 +11,8 @@ sudo apt-get update && sudo apt-get autoremove -y && sudo apt-get clean
 sudo apt-get install clustalx -y
 
 # snapgene and clustalx icons
-[ ! -f main ] && wget -q https://github.com/chenh19/MyWorkspace/archive/refs/heads/main.zip && sleep 1
-unzip -o -q main && sleep 1 && rm main
+[ ! -f main.zip ] && wget -q https://github.com/chenh19/MyWorkspace/archive/refs/heads/main.zip && sleep 1
+unzip -o -q main.zip && sleep 1 && rm main.zip
 sudo cp -rf ./MyWorkspace-main/src/cfg/icon/ /opt/ && sleep 1 && rm -rf ./MyWorkspace-main/
 
 # sysupdate
