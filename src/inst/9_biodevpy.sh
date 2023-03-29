1eae2337e35de2c33dd4e8636acc6446d455fd4e
Hang Chen <chen_h@outlook.com>
2023-03-29 14:37:22 -0500

    Update 9_biodevpy.sh

--- src/inst/9_biodevpy.sh
+++ src/inst/9_biodevpy.sh
@@ -32,20 +32,27 @@ case "$choice" in
         echo -e " \n${TEXT_GREEN}bash <(wget -qO- https://raw.githubusercontent.com/chenh19/MyWorkspace/main/src/cfg/conda/conda.sh)${TEXT_RESET} \n" && sleep 1
         konsole
         
-        # ask whether to install Jupyter Lab
+        # ask whether to install Jupyter Lab or KDevelop
         sudo echo ""
-        read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to install Jupyter Lab? [y/n/c]'$TEXT_RESET)"$' \n' choice
+        read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to install Jupyter Lab (a) or KDevelop (b)? [a/b]'$TEXT_RESET)"$' \n' choice
         case "$choice" in
-          y|Y ) # notify start
+          b|B ) # notify start
+                sudo echo ""
+                echo -e " \n${TEXT_YELLOW}Installing KDevelop...${TEXT_RESET} \n" && sleep 1
+                # install KDevelop
+                # FYI, kdevelop is not dependent on kate
+                sudo apt-get install kate kdevelop -y
+                sudo apt-get install -f -y
+                # notify end
+                echo -e " \n${TEXT_GREEN}KDevelop installed!${TEXT_RESET} \n" && sleep 1;;
+          * )   # notify start
                 sudo echo ""
                 echo -e "${TEXT_YELLOW}Installing Jupyter Lab...${TEXT_RESET} \n" && sleep 1
-                
                 # install Jupyter Lab
                 [ ! -d ./devdeb/ ] && mkdir ./devdeb/
                 wget -q https://github.com/jupyterlab/jupyterlab-desktop/releases/download/v3.6.1-1/JupyterLab-Setup-Debian.deb && echo '"JupyterLab" deb package is downloaded.' && sleep 1 #_to_be_updated
                 mv -f ./*.deb ./devdeb/ && sudo dpkg -i ./devdeb/*.deb && sleep 1
-                sudo apt-get -f -y install
-                
+                sudo apt-get install -f -y
                 # config Jupyter Lab
                 [ ! -d ~/.config/jupyterlab-desktop/ ] && mkdir ~/.config/jupyterlab-desktop/
                 [ ! -d ~/.config/jupyterlab-desktop/lab/ ] && mkdir ~/.config/jupyterlab-desktop/lab/
@@ -53,33 +60,10 @@ case "$choice" in
                 [ ! -d ~/.config/jupyterlab-desktop/lab/user-settings/@jupyterlab/ ] && mkdir ~/.config/jupyterlab-desktop/lab/user-settings/@jupyterlab/
                 [ ! -d ~/.config/jupyterlab-desktop/lab/user-settings/@jupyterlab/apputils-extension/ ] && mkdir ~/.config/jupyterlab-desktop/lab/user-settings/@jupyterlab/apputils-extension/
                 echo -e '{ \n    "theme": "JupyterLab Light", \n    "theme-scrollbars": false, \n    "overrides": { \n        "code-font-family": null, \n        "code-font-size": null, \n        "content-font-family": null, \n        "content-font-size1": null, \n        "ui-font-family": null, \n        "ui-font-size1": null \n    } \n}' > ~/.config/jupyterlab-desktop/lab/user-settings/@jupyterlab/apputils-extension/themes.jupyterlab-settings
-                
                 # notify end
-                echo -e " \n${TEXT_GREEN}Jupyter Lab installed!${TEXT_RESET} \n" && sleep 5;;
-                
-          * ) # notify cancellation
-                echo -e " \n${TEXT_YELLOW}Jupyter Lab not installed.${TEXT_RESET} \n" && sleep 5;;
+                echo -e " \n${TEXT_GREEN}Jupyter Lab installed!${TEXT_RESET} \n" && sleep 1;;
         esac
-        
-        # ask whether to install KDevelop
-        read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to install KDevelop for the C++ and Python developing? [y/n/c]'$TEXT_RESET)"$' \n' choice
-        case "$choice" in
-          y|Y ) # notify start
-                sudo echo ""
-                echo -e " \n${TEXT_YELLOW}Installing KDevelop...${TEXT_RESET} \n" && sleep 1
                 
-                # install KDevelop
-                # FYI, kdevelop is not dependent on kate
-                sudo apt-get install kate kdevelop -y
-                sudo apt-get install -f -y
-                
-                # notify end
-                echo -e " \n${TEXT_GREEN}KDevelop installed!${TEXT_RESET} \n" && sleep 5;;
-                
-          * ) # notify cancellation
-                echo -e " \n${TEXT_YELLOW}KDevelop not installed.${TEXT_RESET} \n" && sleep 5;;
-        esac
-        
         # cleanup
         sudo apt-get autoremove -y && sudo apt-get clean
         [ -d ./devdeb/ ] && rm -rf ./devdeb/
