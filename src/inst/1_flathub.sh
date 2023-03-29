1a88264837e275d905c530a03a1fb2a40d4a5167
Hang Chen <chen_h@outlook.com>
2023-03-29 14:03:49 -0500

    Update 1_flathub.sh

--- src/inst/1_flathub.sh
+++ src/inst/1_flathub.sh
@@ -13,7 +13,7 @@ cd ~/.setup_cache/
 
 # notify start
 sudo echo ""
-echo -e "${TEXT_YELLOW}Installing Flathub...${TEXT_RESET} \n" && sleep 1
+echo -e "${TEXT_YELLOW}Installing Flatpak packages...${TEXT_RESET} \n" && sleep 1
 [ ! -d ~/.local/share/applications ] && mkdir ~/.local/share/applications
 sudo apt-get update && sudo apt-get upgrade -y
 
@@ -117,7 +117,7 @@ sudo snap remove firefox
 sudo apt-get autoremove -y && sudo apt-get clean
 
 # notify end
-echo -e " \n${TEXT_GREEN}Flathub installed!${TEXT_RESET} \n" && sleep 5
+echo -e " \n${TEXT_GREEN}Flatpak packages installed!${TEXT_RESET} \n" && sleep 5
 
 # mark setup.sh
 sed -i 's+bash ./inst/1_flathub.sh+#bash ./inst/1_flathub.sh+g' ~/.setup_cache/setup.sh
