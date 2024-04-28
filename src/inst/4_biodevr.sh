364ad40763a344fd040c636dbfa5f32972c4f788
Hang Chen <chen_h@outlook.com>
2024-04-27 21:33:09 -0500

    Update and rename 5_biodevr.sh to 4_biodevr.sh

--- src/inst/4_biodevr.sh
+++ src/inst/4_biodevr.sh
@@ -21,32 +21,31 @@ case "$choice" in
         sudo apt-get update -qq && sudo apt-get upgrade -y
         [ ! -d ~/Developing/ ] && mkdir ~/Developing/ && kwriteconfig5 --file ~/Developing/.directory --group "Desktop Entry" --key Icon "folder-script"
 
-        # install dependencies
+        ## install dependencies
         sudo apt-get install default-jre default-jdk cmake pandoc libcurl4-openssl-dev libssl-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev libharfbuzz-dev libjpeg-dev libtiff-dev libtiff5-dev libgit2-dev libglpk-dev libnlopt-dev libgeos-dev libxml2-dev texlive-latex-extra -y
 
-        # install R from cran
+        ## install R from cran
         gpg --keyserver keyserver.ubuntu.com --recv-key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
         gpg --armor --export '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7' | sudo tee /etc/apt/trusted.gpg.d/cran_debian_key.asc
         echo -e "deb https://cloud.r-project.org/bin/linux/debian $(lsb_release -cs)-cran40/" | sudo tee /etc/apt/sources.list.d/r-project.list
         sleep 1 && sudo apt-get update -qq && sudo apt-get install r-base littler -y
         sudo R CMD javareconf
 
-        # config posit package manager
+        ## config posit package manager
         if ! grep -q "options(repos = c(CRAN = 'https://packagemanager.posit.co/cran/__linux__/$(lsb_release -cs)/latest'))" /etc/R/Rprofile.site ; then echo -e "options(repos = c(CRAN = 'https://packagemanager.posit.co/cran/__linux__/$(lsb_release -cs)/latest'))" | sudo tee -a /etc/R/Rprofile.site ; fi
 
-        # install R packages
+        ## install R packages
         [ ! -d ./rscript/ ] && mkdir ./rscript/
         echo -e "install.packages(c('devtools', 'BiocManager', 'tidyverse', 'readxl', 'writexl', 'expss', 'vcfR', 'filesstrings', 'R.utils', 'car', 'foreach', 'doParallel', 'rJava', 'RSelenium', 'XML', 'base64enc', 'htmltools', 'markdown', 'rmarkdown', 'ggplot2', 'ggthemes', 'ggpubr', 'ggseqlogo', 'cowplot', 'pheatmap', 'Rtsne', 'umap', 'Seurat', 'workflowr', 'blogdown', 'bookdown', 'svDialogs', 'Rcpp'), force = TRUE, Ncpus = system('nproc --all', intern = TRUE))\nBiocManager::install(c('GenomicRanges','qvalue','DESeq2','EnhancedVolcano','org.Hs.eg.db'), force = TRUE, Ncpus = system('nproc --all', intern = TRUE))" > ./rscript/packages.R
         echo -e "wdman::chrome(version = 'latest')" > ./rscript/webdriver.R
         echo "" && sudo Rscript ./rscript/packages.R
         echo "" && Rscript ./rscript/webdriver.R
         
-        # install RStudio
+        ## install RStudio
         [ ! -d ./devdeb/ ] && mkdir ./devdeb/
         wget -q https://download1.rstudio.org/electron/jammy/amd64/rstudio-2023.12.1-402-amd64.deb && echo '"RStudio" deb package is downloaded.' && sleep 1 #_to_be_updated
         mv -f ./*.deb ./devdeb/ && sudo dpkg -i ./devdeb/*.deb && sleep 1
         sudo apt-get install -f -y
-        # config
         [ ! -d ~/.config/RStudio/ ] && mkdir ~/.config/RStudio/
         kwriteconfig5 --file ~/.config/RStudio/desktop.ini --group General --key view.zoomLevel "1.1"
 
