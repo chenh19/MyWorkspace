dfa697447f4a55a20b470cd7c13b23338e24dbfb
Hang Chen <chen_h@outlook.com>
2024-04-28 20:39:54 -0500

    Update 3_reboot.sh

--- src/cfg/3_reboot.sh
+++ src/cfg/3_reboot.sh
@@ -12,9 +12,9 @@ TEXT_RESET='\e[0m'
 [ -f ~/.setup_cache/setup.sh ] && sed -i 's+bash ./cfg/3_reboot.sh+#bash ./cfg/3_reboot.sh+g' ~/.setup_cache/setup.sh
 
 # final cleanup
+echo -e " \n${TEXT_GREEN}The copies of licenses remain in [~/Licenses/] folder in case you need them later.${TEXT_RESET} \n"
 if [ -d ~/.setup_cache/ ]; then
   # ask whether to delete setup scripts
-  sudo echo ""
   read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to delete the setup scripts in [~/.setup-cache/] folder? [y/n/c]'$TEXT_RESET)"$' \n' choice
   case "$choice" in
     y|Y ) rm -rf ~/.setup_cache/
@@ -23,22 +23,11 @@ if [ -d ~/.setup_cache/ ]; then
   esac
 fi
 
-if [ -d ~/Licenses/ ]; then
-  # ask whether to delete licenses
-  sudo echo ""
-  read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to delete the copies of licenses saved in [~/Licenses/] folder? [y/n/c]'$TEXT_RESET)"$' \n' choice
-  case "$choice" in
-    y|Y ) rm -rf ~/Licenses/
-          echo -e " \n${TEXT_GREEN}The copies of licenses have been removed!${TEXT_RESET} \n" && sleep 1;;
-    * )   echo -e " \n${TEXT_YELLOW}The copies of licenses remain in [~/Licenses/] folder in case you need them later.${TEXT_RESET} \n" && sleep 1;;
-  esac
-fi
-
 # Desktop shortcuts
 bash ~/.shortcut.sh >/dev/null 2>&1
-cp -f /usr/share/applications/org.kde.dolphin.desktop ~/Desktop/Dolphin.desktop && chmod +x ~/Desktop/Dolphin.desktop && sleep 1
-cp -f /usr/share/applications/google-chrome.desktop ~/Desktop/Chrome.desktop && chmod +x ~/Desktop/Chrome.desktop && sleep 1
-echo -e "[Desktop Entry]\nEmptyIcon=user-trash\nIcon=user-trash-full\nName=Trash\nType=Link\nURL[$e]=trash:/" > ~/Desktop/Trash.desktop && sleep 1
+cp -f /usr/share/applications/org.kde.dolphin.desktop ~/Desktop/Dolphin.desktop && chmod +x ~/Desktop/Dolphin.desktop && sleep 3
+cp -f /usr/share/applications/google-chrome.desktop ~/Desktop/Chrome.desktop && chmod +x ~/Desktop/Chrome.desktop && sleep 3
+echo -e "[Desktop Entry]\nEmptyIcon=user-trash\nIcon=user-trash-full\nName=Trash\nType=Link\nURL[$e]=trash:/" > ~/Desktop/Trash.desktop && sleep 3
 echo -e "## Personalization:\n- [ ] Right click on desktop > Configure Desktop and Wallpaper > change wallpaper\n- [ ] System Settings > Workspace Behavior > Screen Locking > Appearance > Configue > change lock screen\n- [ ] System Settings > Startup and Shutdown > Login Screen (SDDM) > select the first one and change background\n- [ ] System Settings > Users > change avatar" > ~/Desktop/Personalization.md
 sleep 1
 
