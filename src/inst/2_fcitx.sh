4e93ec9e585564472c5b41dee77ba89ae9fe09b8
Hang Chen <chen_h@outlook.com>
2024-08-09 00:39:50 -0500

    Update 2_fcitx.sh

--- src/inst/2_fcitx.sh
+++ src/inst/2_fcitx.sh
@@ -29,7 +29,7 @@ sudo apt-get --fix-missing update && sudo apt-get install -f -y
 [ ! -f fcitx5-themes.zip ] && wget -q "https://www.dropbox.com/scl/fi/7fldgym73qz3oq88z4ruh/fcitx5-themes.zip?rlkey=y9ko399f3pxkmne2mdkbxgxo9" -O fcitx5-themes.zip && sleep 1
 unzip -o -q fcitx5-themes.zip -d ./cfg/ && sleep 1 && rm -f fcitx5-themes.zip && sleep 1
 cp -rf ./cfg/fcitx5-themes/* ~/.local/share/fcitx5/themes/ && sleep 1
-        
+
 ## config fcitx (takes effect after rebooting)
 cp -f /usr/share/applications/org.fcitx.Fcitx5.desktop ~/.config/autostart/ && sudo chmod +x ~/.config/autostart/org.fcitx.Fcitx5.desktop
 cp -rf ./cfg/fcitx5/* ~/.config/fcitx5/
@@ -38,14 +38,14 @@ echo -e "${TEXT_YELLOW}> click ${TEXT_GREEN}[OK]${TEXT_YELLOW} and then ${TEXT_G
 echo -e "${TEXT_YELLOW}> select ${TEXT_GREEN}[activate Flexible Input Method Framework v5 (fcitx5)]${TEXT_YELLOW} and click ${TEXT_GREEN}[OK]${TEXT_RESET}"
 echo -e "${TEXT_YELLOW}> click ${TEXT_GREEN}[OK]${TEXT_YELLOW} to finish${TEXT_RESET} \n" && sleep 1
 im-config
-        
+
 ## config locales
 echo -e " \n${TEXT_YELLOW}Please use [Space] to select ${TEXT_GREEN}zh_CN.UTF-8${TEXT_YELLOW} and then [Enter] to continue.${TEXT_RESET} \n" && sleep 5
 sudo dpkg-reconfigure locales
-        
+
 ## config environment
 echo -e 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"\nGTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx' | sudo tee /etc/environment
-        
+
 # cleanup
 rm -rf ./cfg/fcitx5-themes/
 sudo apt-get update -qq && sudo apt-get autoremove -y && sudo apt-get clean
