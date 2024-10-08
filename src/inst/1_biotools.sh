030aca7500557f0e32df1ed74eda974697a23f65
Hang Chen <chen_h@outlook.com>
2024-10-08 12:36:38 -0500

    Update and rename 3_biodaily.sh to 1_biotools.sh

--- src/inst/1_biotools.sh
+++ src/inst/1_biotools.sh
@@ -13,13 +13,14 @@ cd ~/.setup_cache/
 
 # ask whether to install SnapGene/IGV/PyMOL
 sudo echo ""
-read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to install daily biological tools, such as SnapGene and PyMOL? [y/n/c]'$TEXT_RESET)"$' \n' choice
+read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Would you like to install bioinformatics tools? [y/n/c]'$TEXT_RESET)"$' \n' choice
 case "$choice" in
   y|Y ) # notify start
         sudo echo ""
-        echo -e "${TEXT_YELLOW}Installing daily biological tools (SnapGene/IGV/PyMOL/FastQC)...${TEXT_RESET} \n" && sleep 1
+        echo -e "${TEXT_YELLOW}Installing bioinformatics tools...${TEXT_RESET} \n" && sleep 1
         sudo apt-get update -qq && sudo apt-get upgrade -y
         if ! dpkg -l | grep -q "^ii.*wget" ; then sudo apt-get update -qq && sudo apt-get install wget -y && sleep 1 ; fi
+        [ ! -d ~/Developing/ ] && mkdir ~/Developing/ && kwriteconfig5 --file ~/Developing/.directory --group "Desktop Entry" --key Icon "folder-script"
         
         ## install PyMOL/FastQC/Meld
         [ ! -d ~/igv ] && mkdir ~/igv/
@@ -59,8 +60,40 @@ case "$choice" in
             --remove-key 'Categories' --add-category 'Science;' \
         /usr/share/applications/igv.desktop
         rm -rf ./IGV_Linux_*/
+
+        ## install R
+        ### install dependencies
+        sudo apt-get install default-jre default-jdk cmake pandoc libcurl4-openssl-dev libssl-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev libharfbuzz-dev libjpeg-dev libtiff-dev libtiff5-dev libgit2-dev libglpk-dev libnlopt-dev libgeos-dev libxml2-dev texlive-latex-extra -y
+
+        ### install R from cran
+        gpg --keyserver keyserver.ubuntu.com --recv-key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
+        gpg --armor --export '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7' | sudo tee /etc/apt/trusted.gpg.d/cran_debian_key.asc
+        echo -e "deb https://cloud.r-project.org/bin/linux/debian $(lsb_release -cs)-cran40/" | sudo tee /etc/apt/sources.list.d/r-project.list
+        sleep 1 && sudo apt-get update -qq && sudo apt-get install r-base littler -y
+        sudo R CMD javareconf
+
+        ### config posit package manager
+        if ! grep -q "options(repos = c(CRAN = 'https://packagemanager.posit.co/cran/__linux__/$(lsb_release -cs)/latest'))" /etc/R/Rprofile.site ; then echo -e "options(repos = c(CRAN = 'https://packagemanager.posit.co/cran/__linux__/$(lsb_release -cs)/latest'))" | sudo tee -a /etc/R/Rprofile.site ; fi
+
+        ### install R packages
+        [ ! -d ./rscript/ ] && mkdir ./rscript/
+        echo -e "install.packages(c('devtools', 'BiocManager', 'tidyverse', 'readxl', 'writexl', 'expss', 'vcfR', 'filesstrings', 'R.utils', 'car', 'foreach', 'doParallel', 'rJava', 'RSelenium', 'XML', 'base64enc', 'htmltools', 'markdown', 'rmarkdown', 'ggplot2', 'ggthemes', 'ggpubr', 'ggseqlogo', 'cowplot', 'pheatmap', 'Rtsne', 'umap', 'Seurat', 'workflowr', 'blogdown', 'bookdown', 'svDialogs', 'Rcpp'), force = TRUE, Ncpus = system('nproc --all', intern = TRUE))\nBiocManager::install(c('GenomicRanges','qvalue','DESeq2','EnhancedVolcano','org.Hs.eg.db'), force = TRUE, Ncpus = system('nproc --all', intern = TRUE))" > ./rscript/packages.R
+        echo -e "wdman::chrome(version = 'latest')" > ./rscript/webdriver.R
+        echo "" && sudo Rscript ./rscript/packages.R
+        echo "" && Rscript ./rscript/webdriver.R
+        
+        ### install RStudio
+        [ ! -d ./devdeb/ ] && mkdir ./devdeb/
+        wget -q "https://www.dropbox.com/scl/fi/3j0gkfvl21wsetqeyxf4d/rstudio.deb?rlkey=2lq1ezrjb39yrfl6hxq9qmn2v" -O rstudio.deb && echo '"RStudio" deb package is downloaded.' && sleep 1
+        mv -f ./*.deb ./devdeb/ && sudo dpkg -i ./devdeb/*.deb && sleep 1
+        sudo apt-get install -f -y
+        [ ! -d ~/.config/RStudio/ ] && mkdir ~/.config/RStudio/
+        kwriteconfig5 --file ~/.config/RStudio/desktop.ini --group General --key view.zoomLevel "1.1"
         
         # cleanup
+        [ -f ./.Rhistory ] && rm -f ./.Rhistory
+        [ -d ./rscript/ ] && rm -rf ./rscript/
+        [ -d ./devdeb/ ] && rm -rf ./devdeb/
         sudo apt-get update -qq && sudo apt-get autoremove -y && sudo apt-get clean
 
         # notify end
@@ -73,4 +106,4 @@ esac
 
 
 # mark setup.sh
-[ -f ~/.setup_cache/setup.sh ] && sed -i 's+bash ./inst/3_biodaily.sh+#bash ./inst/3_biodaily.sh+g' ~/.setup_cache/setup.sh
+[ -f ~/.setup_cache/setup.sh ] && sed -i 's+bash ./inst/1_biotools.sh+#bash ./inst/1_biotools.sh+g' ~/.setup_cache/setup.sh
