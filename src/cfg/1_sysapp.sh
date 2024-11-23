1bd42774a5c46b852da03c0621c786d0acc21f7c
Hang Chen <chen_h@outlook.com>
2024-11-23 17:03:55 -0600

    Update 1_sysapp.sh

--- src/cfg/1_sysapp.sh
+++ src/cfg/1_sysapp.sh
@@ -342,6 +342,7 @@ rm -rf ./fonts/
 ######################################################################################
 
 # command alias
+echo ""
 ## poweroff
 if ! grep -q "alias reboot='systemctl reboot'" ~/.bashrc ; then echo -e "alias reboot='systemctl reboot'" >> ~/.bashrc ; fi
 if ! grep -q "alias poweroff='systemctl poweroff'" ~/.bashrc ; then echo -e "alias poweroff='systemctl poweroff'" >> ~/.bashrc ; fi
