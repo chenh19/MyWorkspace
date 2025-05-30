ebc1aa4a8cbebcadd5aa09b0646f0c07ea7964a1
Hang Chen <chen_h@outlook.com>
2025-05-30 11:43:43 -0500

    Update 0_systools.sh

--- src/inst/0_systools.sh
+++ src/inst/0_systools.sh
@@ -190,6 +190,8 @@ if ! dpkg -l | grep -q "^ii.*wget" ; then sudo apt-get update -qq && sudo apt-ge
     --set-key 'Exec' --set-value 'bash /opt/Thunderbird/thunderbird.sh' \
     --remove-key 'Categories' --add-category 'Network;' \
   /usr/share/applications/thunderbird.desktop
+  sleep 1
+  cp -f /usr/share/applications/thunderbird.desktop ~/.config/autostart/ && sudo chmod +x ~/.config/autostart/thunderbird.desktop
 
 # cleanup
 rm -rf ./deb/
