efecd3653c9db3df2575d8f3e92a027a698f98d4
Hang Chen <chen_h@outlook.com>
2025-08-22 01:51:27 -0500

    Update 1_sysapp.sh

--- src/cfg/1_sysapp.sh
+++ src/cfg/1_sysapp.sh
@@ -53,8 +53,9 @@ kwriteconfig5 --file ~/.config/klipperrc --group 'General' --key 'KeepClipboardC
 # kde browser integration reminder hide
 kwriteconfig5 --file ~/.config/kded5rc --group 'Module-browserintegrationreminder' --key 'autoload' "false"
 
-# Opacity > Translucent
+# Opacity > Translucent; no Floating
 kwriteconfig5 --file ~/.config/plasmashellrc --group 'PlasmaViews' --group 'Panel 2' --key panelOpacity "1"
+kwriteconfig5 --file ~/.config/plasmashellrc --group 'PlasmaViews' --group 'Panel 2' --key floating "0"
 
 ######################################################################################
 
