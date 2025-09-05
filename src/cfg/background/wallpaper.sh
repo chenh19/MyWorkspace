0a86e2559f8669292fe3eaaf0ac3b1dc8b71a283
Hang Chen <chen_h@outlook.com>
2025-09-05 10:59:56 -0500

    Update wallpaper.sh

--- src/cfg/background/wallpaper.sh
+++ src/cfg/background/wallpaper.sh
@@ -3,12 +3,14 @@
 # select image and make a copy
 src="$1"
 srcname=$(basename "$src")
+filename="${srcname// /_}"
+file="$HOME/.config/background/$filename"
 mkdir -p $HOME/.config/background/
-cp -f "$src" $HOME/.config/background/
+cp -f "$src" "$file"
 
 # set wallpaper
-plasma-apply-wallpaperimage "$HOME/.config/background/$srcname"
+plasma-apply-wallpaperimage "$file"
 
 # set lock screen
-kwriteconfig6 --file ~/.config/kscreenlockerrc --group 'Greeter' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'Image' --type string "$HOME/.config/background/$srcname"
-kwriteconfig6 --file ~/.config/kscreenlockerrc --group 'Greeter' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'PreviewImage' --type string "$HOME/.config/background/$srcname"
+kwriteconfig6 --file ~/.config/kscreenlockerrc --group 'Greeter' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'Image' --type string "$file"
+kwriteconfig6 --file ~/.config/kscreenlockerrc --group 'Greeter' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'PreviewImage' --type string "$file"
