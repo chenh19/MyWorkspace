383341084b30ffee159eda9621ea047083b07a50
Hang Chen <chen_h@outlook.com>
2025-09-05 11:10:09 -0500

    Update sddm.sh

--- src/cfg/background/sddm.sh
+++ src/cfg/background/sddm.sh
@@ -3,13 +3,14 @@
 # select image and make a copy
 src="$1"
 srcname=$(basename "$src")
+filename="${srcname// /_}"
 mkdir -p /usr/share/sddm/themes/breeze/
-cp -f "$src" /usr/share/sddm/themes/breeze/
-chmod 644 "/usr/share/sddm/themes/breeze/$srcname"
+cp -f "$src" "/usr/share/sddm/themes/breeze/"
+chmod 644 "/usr/share/sddm/themes/breeze/$filename"
 mkdir -p /usr/share/sddm/themes/debian-breeze/
-cp -f "$src" /usr/share/sddm/themes/debian-breeze/
-chmod 644 "/usr/share/sddm/themes/debian-breeze/$srcname"
+cp -f "$src" "/usr/share/sddm/themes/debian-breeze/"
+chmod 644 "/usr/share/sddm/themes/debian-breeze/$filename"
 
 # set sddm
-kwriteconfig6 --file /usr/share/sddm/themes/breeze/theme.conf --group General --key 'background' --type string "$srcname"
-kwriteconfig6 --file /usr/share/sddm/themes/debian-breeze/theme.conf --group General --key 'background' --type string "$srcname"
+kwriteconfig6 --file /usr/share/sddm/themes/breeze/theme.conf --group General --key 'background' --type string "$filename"
+kwriteconfig6 --file /usr/share/sddm/themes/debian-breeze/theme.conf --group General --key 'background' --type string "$filename"
