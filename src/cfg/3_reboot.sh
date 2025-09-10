c094e32e919bc290c0d73eacdb88f708ef754096
Hang Chen <chen_h@outlook.com>
2025-09-09 23:38:01 -0500

    Update 3_reboot.sh

--- src/cfg/3_reboot.sh
+++ src/cfg/3_reboot.sh
@@ -15,7 +15,7 @@ TEXT_RESET="$(tput sgr0)"
 [ -d ~/.setup_cache/ ] && rm -rf ~/.setup_cache/
 
 # notify end
-echo -e "${TEXT_GREEN}All done! The copies of licenses remain in [~/Licenses/] folder in case you need them later.${TEXT_RESET}\n\n" && sleep 1
+echo -e "\n${TEXT_GREEN}All done! The copies of licenses remain in [~/Licenses/] folder in case you need them later.${TEXT_RESET}\n\n" && sleep 1
 
 # reboot
 read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'System reboot required, would you like to reboot the system now? [y/n]'$TEXT_RESET)"$'\n' choice
