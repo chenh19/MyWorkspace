8df42505ffd43e29707a0b5fcd0f8a74ca6f8876
Hang Chen <chen_h@outlook.com>
2026-01-23 00:06:25 -0600

    Update 2_sysdsp.sh

--- src/cfg/2_sysdsp.sh
+++ src/cfg/2_sysdsp.sh
@@ -282,11 +282,6 @@ echo -e "\n${TEXT_YELLOW}Finalizing...${TEXT_RESET}\n" && sleep 1
 [ -f ~/.scale.sh ] && bash ~/.scale.sh >/dev/null 2>&1
 [ -f ~/.shortcut.sh ] && bash ~/.shortcut.sh >/dev/null 2>&1
 [ -f ~/.size-restore.sh ] && bash ~/.size-restore.sh >/dev/null 2>&1
-if grep -q "plugin=org.kde.plasma.showdesktop" ~/.config/plasma-org.kde.plasma.desktop-appletsrc; then
-    cp -f /usr/share/applications/org.kde.dolphin.desktop ~/Desktop/Dolphin.desktop && chmod +x ~/Desktop/Dolphin.desktop && sleep 1
-    cp -f /usr/share/applications/google-chrome.desktop ~/Desktop/Chrome.desktop && chmod +x ~/Desktop/Chrome.desktop && sleep 1
-    echo -e "[Desktop Entry]\nEmptyIcon=user-trash\nIcon=user-trash-full\nName=Trash\nType=Link\nURL[\$e]=trash:/" > ~/Desktop/Trash.desktop && sleep 1
-fi
 
 # mark setup.sh
 [ -f ~/.setup_cache/setup.sh ] && sed -i 's+bash ./cfg/2_sysdsp.sh+#bash ./cfg/2_sysdsp.sh+g' ~/.setup_cache/setup.sh
