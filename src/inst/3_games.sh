cd2b6e99c0d7f822121b8c06c3f558909dc8b9fd
Hang Chen <chen_h@outlook.com>
2026-05-07 02:55:28 -0500

    Update 3_games.sh

--- src/inst/3_games.sh
+++ src/inst/3_games.sh
@@ -25,7 +25,6 @@ case "$choice" in
         sudo dpkg --add-architecture i386
         sudo apt update -qq && sudo apt install steam-installer mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 -y
         #post installation:
-        #sudo cp -f  ~/.setup_cache/cfg/steam/steam.desktop /usr/share/applications/steam-client.desktop
         #[ -f ~/.steam/debian-installation/deb-installer/steam.desktop ] && sudo cp -f ~/.steam/debian-installation/deb-installer/steam.desktop /usr/share/applications/steam-client.desktop
         #[ -f /usr/share/applications/steam-client.desktop ] && sudo chmod 644 /usr/share/applications/steam-client.desktop
         #[ -f /usr/share/applications/steam.desktop ] && sudo rm -f /usr/share/applications/steam.desktop
